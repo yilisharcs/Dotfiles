5dd18caff447d874eaf95a947ea3342988960857
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-03-21 11:48:02 -0300

    minor stuff

--- nvim/.config/nvim/lua/custom/plugins/spider.lua
+++ nvim/.config/nvim/lua/custom/plugins/spider.lua
@@ -5,7 +5,7 @@ return {
     keys = {
       { 'w',  '<CMD>lua require("spider").motion("w")<CR>',  mode = { 'n', 'x', 'o' } },
       { 'e',  '<CMD>lua require("spider").motion("e")<CR>',  mode = { 'n', 'x', 'o' } },
-      { 'b',  function() require("spider").motion("b") end,  mode = { 'n', 'x', 'o' }, },
+      { 'b',  function() require("spider").motion("b") end,  mode = { 'n', 'x', 'o' } },
       { 'ge', '<CMD>lua require("spider").motion("ge")<CR>', mode = { 'n', 'x', 'o' } },
       -- -- won't load if typed too fast the first time. likely an issue with how lazy handles remap=true maps
       -- { '<C-w>', '<ESC>cvb', mode = 'i', remap = true },
