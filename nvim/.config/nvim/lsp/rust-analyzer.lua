a8a2e7b9f76d02e19e7d096b6999770fc61ec537
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-03-28 11:08:45 -0300

    update lsp config

---
+++ nvim/.config/nvim/lsp/rust-analyzer.lua
@@ -0,0 +1,10 @@
+return {
+  cmd = { 'rustup', 'run', 'stable', 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml' },
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = { allFeatures = true }
+    }
+  },
+}
