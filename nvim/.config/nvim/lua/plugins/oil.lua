127b1ed7f70b7860eee043c48f5415b6a4fe6f42
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-05 18:22:52 -0300

    set lunaperche as default colorscheme

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -12,17 +12,20 @@ return {
           return package.loaded['nvim-web-devicons']
         end
 
-        local hi = vim.api.nvim_set_hl
-
-        hi(0, 'MiniIconsAzure', { fg = '#74c7ec' })
-        hi(0, 'MiniIconsBlue', { fg = '#89b4fa' })
-        hi(0, 'MiniIconsCyan', { fg = '#94e2d5' })
-        hi(0, 'MiniIconsGreen', { fg = '#a6e3a1' })
-        hi(0, 'MiniIconsGrey', { fg = '#cdd6f4' })
-        hi(0, 'MiniIconsOrange', { fg = '#fab387' })
-        hi(0, 'MiniIconsPurple', { fg = '#cba6f7' })
-        hi(0, 'MiniIconsRed', { fg = '#f38ba8' })
-        hi(0, 'MiniIconsYellow', { fg = '#f9e2af' })
+        vim.api.nvim_create_autocmd('ColorScheme', {
+          group    = vim.api.nvim_create_augroup('Mini_Icons', { clear = true }),
+          callback = function()
+            vim.api.nvim_set_hl(0, 'MiniIconsAzure', { fg = '#74c7ec' })
+            vim.api.nvim_set_hl(0, 'MiniIconsBlue', { fg = '#89b4fa' })
+            vim.api.nvim_set_hl(0, 'MiniIconsCyan', { fg = '#94e2d5' })
+            vim.api.nvim_set_hl(0, 'MiniIconsGreen', { fg = '#a6e3a1' })
+            vim.api.nvim_set_hl(0, 'MiniIconsGrey', { fg = '#cdd6f4' })
+            vim.api.nvim_set_hl(0, 'MiniIconsOrange', { fg = '#fab387' })
+            vim.api.nvim_set_hl(0, 'MiniIconsPurple', { fg = '#cba6f7' })
+            vim.api.nvim_set_hl(0, 'MiniIconsRed', { fg = '#f38ba8' })
+            vim.api.nvim_set_hl(0, 'MiniIconsYellow', { fg = '#f9e2af' })
+          end
+        })
       end,
     }
   },
