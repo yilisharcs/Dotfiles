15a510dd721a91b82955b241e304e8377da14c42
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-06 17:16:30 -0300

    turns out fidget is useful and i don't like virtual line diagnostics

---
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,17 @@
+return {
+  'j-hui/fidget.nvim',
+  event = 'LspAttach',
+  opts = {
+    progress = {
+      display = {
+        done_icon = '✔  ',
+        progress_icon = { pattern = 'moon' },
+      },
+    },
+    notification = {
+      window = {
+        winblend = 0,
+      },
+    }
+  }
+}
