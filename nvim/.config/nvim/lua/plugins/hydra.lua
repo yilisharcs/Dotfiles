d12d1417dc28b316c75b8985d4fa40ffd8b951d1
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-17 13:03:45 -0300

    fix hydras

--- nvim/.config/nvim/lua/plugins/hydra.lua
+++ nvim/.config/nvim/lua/plugins/hydra.lua
@@ -10,37 +10,33 @@ return {
         mode = 'n',
         body = 'z',
         heads = {
-          { 'h',     '5zh', {} },
-          { 'l',     '5zl', { desc = '<= =>' } },
-          { 'H',     'zH',  {} },
-          { 'L',     'zL',  { desc = 'Half screen <= =>' } },
-          { '<C-c>', nil,   { exit = true } },
+          { 'h', '5zh', {} },
+          { 'l', '5zl', { desc = '<= =>' } },
+          { 'H', 'zH',  {} },
+          { 'L', 'zL',  { desc = 'Half screen <= =>' } },
         }
       })
 
       hydra({
         name = 'WINCMD',
+        hint = [[
+ ^ ^ ^ ^^^Wincmd^^^^^^^^^ ^
+ ^ ^ _+_ ^ ^  ^ ^ _K_ ^ ^ ^
+ _<_ _=_ _>_  _H_ _c_ _L_ ^
+ ^ ^ _-_ ^ ^  ^ ^ _J_ ^ ^ ^]],
         mode = 'n',
         body = '<C-w>',
         heads = {
-          { '<',     '<C-w>2<',            {} },
-          { '>',     '<C-w>2>',            { desc = 'Horizontal resize' } },
-          { '-',     '<C-w>2-',            {} },
-          { '+',     '<C-w>2+',            { desc = 'Vertical resize' } },
-          { 'h',     '<C-w>h',             {} },
-          { 'l',     '<C-w>l',             {} },
-          { 'j',     '<C-w>j',             {} },
-          { 'k',     '<C-w>k',             { desc = 'Jump' } },
-          { 'H',     '<C-w>H',             {} },
-          { 'L',     '<C-w>L',             {} },
-          { 'J',     '<C-w>J',             {} },
-          { 'K',     '<C-w>K',             { desc = 'Move' } },
-          { 't',     '<CMD>tab split<CR>', {} },
-          { 's',     '<C-w>s',             {} },
-          { 'v',     '<C-w>v',             { desc = 'Split' } },
-          { '=',     '<C-w>=',             {} },
-          { 'c',     '<C-w>c',             { desc = 'Close' } },
-          { '<C-c>', nil,                  { exit = true } },
+          { '<', '<C-w>2<' },
+          { '>', '<C-w>2>' },
+          { '-', '<C-w>2-' },
+          { '+', '<C-w>2+' },
+          { '=', '<C-w>=' },
+          { 'H', '<C-w>H' },
+          { 'L', '<C-w>L' },
+          { 'J', '<C-w>J' },
+          { 'K', '<C-w>K' },
+          { 'c', '<C-w>c' },
         },
       })
     end,
