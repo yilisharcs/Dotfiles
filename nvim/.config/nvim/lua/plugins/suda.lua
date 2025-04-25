df91441c462e2d18a56fb1a6ebada6fbfd015a4b
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-25 20:11:52 -0300

    update plugins

---
+++ nvim/.config/nvim/lua/plugins/suda.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'lambdalisue/suda.vim',
+    init = function()
+      vim.cmd([[
+        cnoreabbrev <expr> sudo (getcmdtype() ==# ':' && getcmdline() =~# '^sudo') ? 'SudaWrite' : 'sudo'
+      ]])
+    end
+  }
+}
