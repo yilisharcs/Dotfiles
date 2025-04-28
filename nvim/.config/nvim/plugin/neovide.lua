37aabb2674876e2e18940fd737e8bf19664708b0
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-28 14:26:22 -0300

    feat: add emoji guifont as fallback

--- nvim/.config/nvim/plugin/neovide.lua
+++ nvim/.config/nvim/plugin/neovide.lua
@@ -7,7 +7,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
 
-  vim.go.guifont = 'JetBrainsMono Nerd Font Mono:h13'
+  vim.go.guifont = 'JetBrainsMono_Nerd_Font_Mono,Noto_Color_Emoji:h13'
   vim.g.neovide_scale_factor = 1.0
   vim.keymap.set({ 'n', 't' }, '<C-->', '<CMD>let g:neovide_scale_factor-=0.1<CR>')
   vim.keymap.set({ 'n', 't' }, '<C-=>', '<CMD>let g:neovide_scale_factor+=0.1<CR>')
