047fe9bbe69d349b4c3022a30dc1d272a9febe6a
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-28 17:18:54 -0300

    fix: use greedy pattern matching for TermClose event

--- nvim/.config/nvim/plugin/neovide.lua
+++ nvim/.config/nvim/plugin/neovide.lua
@@ -68,7 +68,7 @@ if vim.g.neovide then
       " Set default cwd if opened with no files as arguments
       au UIEnter * if argc(-1) == 0 | cd ~/.dotfiles | endif
       " Close terminal buffers as if {cmd} wasn't supplied to :term
-      au TermClose *:nu*,*/bin/bash* silent! execute 'bdelete! '.expand('<abuf>')
+      au TermClose * silent! execute 'bdelete! '.expand('<abuf>')
     augroup END
   ]])
 end
