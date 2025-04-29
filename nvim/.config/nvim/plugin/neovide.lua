a70bae56a26f874a4f1fa9b597e28a92fed5ae95
yilisharcs <200692511+yilisharcs@users.noreply.github.com>
2025-04-29 15:08:56 -0300

    fix: set winblend to magic number to fix unwanted transparency in floating windows

--- nvim/.config/nvim/plugin/neovide.lua
+++ nvim/.config/nvim/plugin/neovide.lua
@@ -6,6 +6,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_smooth_blink = true
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
+  vim.o.winblend = 1
 
   vim.go.guifont = 'JetBrainsMono_Nerd_Font_Mono,Noto_Color_Emoji:h13'
   vim.g.neovide_scale_factor = 1.0
