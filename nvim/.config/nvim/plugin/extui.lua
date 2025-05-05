d12d684ad1acd2f541bf68608ca01af597682ad3
yilisharcs <yilisharcs@gmail.com>
2025-05-05 08:48:56 -0300

    hack: create and delete window to get rid of starting bar on top of the screen
    Problem:
    https://github.com/neovide/neovide/issues/3125
    
    Solution:
    autocmd to create and destroy a window

--- nvim/.config/nvim/plugin/extui.lua
+++ nvim/.config/nvim/plugin/extui.lua
@@ -7,3 +7,14 @@ require('vim._extui').enable({
 })
 
 vim.o.cmdheight = 0
+
+if vim.g.neovide then
+  vim.api.nvim_create_autocmd('FocusGained', {
+    group = vim.api.nvim_create_augroup('Neovide_ExTUI', { clear = false }),
+    pattern = '*',
+    once = true,
+    callback = function()
+      vim.cmd('new | close')
+    end
+  })
+end
