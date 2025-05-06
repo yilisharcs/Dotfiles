4e24864ef372a54312fce94b043c87dae384dd7f
yilisharcs <yilisharcs@gmail.com>
2025-05-06 18:08:45 -0300

    feat: friendship ended with fidget, now snacks is my best friend

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    notifier = { enabled = true },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd('LspProgress', {
+      ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+      callback = function(ev)
+        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+        vim.notify(vim.lsp.status(), vim.log.levels.INFO, {
+          id = 'lsp_progress',
+          title = 'LSP Progress',
+          opts = function(notif)
+            notif.icon = ev.data.params.value.kind == 'end' and ' '
+              or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+          end
+        })
+      end
+    })
+  end
+}
