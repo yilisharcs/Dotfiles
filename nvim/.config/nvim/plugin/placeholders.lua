130d7c5a129c34afd3cbe90fae2c812ea4f57288
yilisharcs <yilisharcs@gmail.com>
2025-05-06 10:30:00 -0300

    feat: placeholder skeletons

---
+++ nvim/.config/nvim/plugin/placeholders.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', ',gnu', 'ggdG:0read $XDG_CONFIG_HOME/nvim/skeletons/license/gpl-3.0.txt | $d_<CR>gg')
+vim.keymap.set('n', ',lorem', 'ggdG:0read $XDG_CONFIG_HOME/nvim/skeletons/lorem/ipsum.txt | $d_<CR>gg')
