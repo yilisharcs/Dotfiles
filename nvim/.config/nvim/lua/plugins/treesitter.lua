4c9b700fa1578adcc7a09e2de77100ec677f3dcf
yilisharcs <yilisharcs@gmail.com>
2025-05-19 16:57:42 -0300

    more treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,13 @@ return {
   dependencies = {
     'RRethy/nvim-treesitter-endwise',
     'HiPhish/rainbow-delimiters.nvim',
-    -- { 'nvim-treesitter/nvim-treesitter-context', opts = { min_window_height = 20 } },
+    {
+      'nvim-treesitter/nvim-treesitter-context',
+      opts = {
+        min_window_height = 20,
+        separator = '⥰',
+      }
+    },
   },
   build = ':TSUpdate',
   cond = not vim.g.shell_editor == true,
@@ -27,7 +33,7 @@ return {
       additional_vim_regex_highlighting = false,
     })
 
-    vim.keymap.set('n', '<M-u>', '<CMD>TSBufToggle highlight<CR>', { desc = '[TS] Toggle highlights' })
+    vim.keymap.set('n', '<M-u>', '<CMD>TSBufToggle highlight | TSContextToggle<CR>', { desc = '[TS] Toggle highlights' })
     vim.keymap.set('n', '<leader><F9>', '<CMD>InspectTree<CR>', { desc = '[TS] Inspect tree' })
 
     vim.cmd([[
