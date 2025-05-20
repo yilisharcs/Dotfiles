26dadbb1bb1d0123ec2bf4c08eee7c5bbec4f6c1
yilisharcs <yilisharcs@gmail.com>
2025-05-20 09:58:29 -0300

    add auto root trigger for jj

--- nvim/.config/nvim/lua/plugins/mini-misc.lua
+++ nvim/.config/nvim/lua/plugins/mini-misc.lua
@@ -3,7 +3,7 @@ return {
   version = false,
   event = 'BufReadPost [^:]*',
   config = function()
-    require('mini.misc').setup_auto_root({ '.git', 'Makefile' })
+    require('mini.misc').setup_auto_root({ '.git', '.jj', 'Makefile' })
     require('mini.misc').setup_restore_cursor({ center = false })
   end
 }
