42a7e56bea04658e625f27c23d0256725ff0e649
yilisharcs <yilisharcs@gmail.com>
2025-05-27 10:48:37 -0300

    changed my mind about the conceal

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -3,7 +3,14 @@ return {
   -- enabled = false,
   ft = { 'markdown', 'codecompanion' },
   opts = {
-    anti_conceal = { enabled = false },
+    render_modes = true,
+    anti_conceal = {
+      enabled = true,
+      above = 1,
+      below = 1,
+    },
+    pipe_table = { preset = 'round' },
+    html = { comment = { conceal = false } },
     sign = { enabled = false },
   }
 }
