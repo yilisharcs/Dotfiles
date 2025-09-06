6c80c0a95373eac645cce6980057edf4e9b8cfd6
yilisharcs <yilisharcs@gmail.com>
2025-09-06 18:22:04 -0300

    chore: ordering

--- nvim/.config/nvim/lua/plugins/mini-hipatterns.lua
+++ nvim/.config/nvim/lua/plugins/mini-hipatterns.lua
@@ -5,12 +5,12 @@ return {
   config = function()
     require("mini.hipatterns").setup({
       highlighters = {
+        hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
         fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
         hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
         todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-        todo_rust_macro = { pattern = "todo!%(%)", group = "MiniHipatternsFixme" },
         note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-        hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+        todo_rust_macro = { pattern = "todo!%(%)", group = "MiniHipatternsFixme" },
       },
     })
   end
