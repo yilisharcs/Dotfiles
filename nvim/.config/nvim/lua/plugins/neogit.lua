37b3b06262dc8d45d94a46f4a230b08d5419ce9f
yilisharcs <yilisharcs@gmail.com>
2025-09-06 15:28:41 -0300

    nvim plugin diffview
    Note: trying this again

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,6 +1,9 @@
 return {
   "NeogitOrg/neogit",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
   event = "CmdlineEnter",
   keys = {
     { "<leader>i", "<CMD>Neogit<CR>" },
@@ -15,5 +18,42 @@ return {
   end,
   opts = {
     graph_style = "unicode",
-  }
+  },
+  config = function(_, opts)
+    require("neogit").setup(opts)
+
+    local actions = require("diffview.actions")
+
+    require("diffview").setup({
+      -- view = {
+      --   default = {
+      --     layout = "diff2_vertical"
+      --   }
+      -- },
+      file_panel = {
+        win_config = {
+          position = "right",
+          -- width = math.floor(vim.o.columns * 0.33 + 0.5),
+        },
+      },
+      keymaps = {
+        view = {
+          { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+          { "n", "<C-p>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
+        },
+        file_panel = {
+          { "n", "<C-n>", actions.select_next_entry,  { desc = "Open the diff for the next file" } },
+          { "n", "<C-p>", actions.select_prev_entry,  { desc = "Open the diff for the previous file" } },
+          { "n", "<c-k>", actions.scroll_view(-0.25), { desc = "Scroll the view up" } },
+          { "n", "<c-j>", actions.scroll_view(0.25),  { desc = "Scroll the view down" } },
+        },
+        file_history_panel = {
+          { "n", "<C-n>", actions.select_next_entry,  { desc = "Open the diff for the next file" } },
+          { "n", "<C-p>", actions.select_prev_entry,  { desc = "Open the diff for the previous file" } },
+          { "n", "<c-k>", actions.scroll_view(-0.25), { desc = "Scroll the view up" } },
+          { "n", "<c-j>", actions.scroll_view(0.25),  { desc = "Scroll the view down" } },
+        },
+      },
+    })
+  end
 }
