a2a97f070569ee3e36d06d1f7c022dd39eaabada
yilisharcs <yilisharcs@gmail.com>
2025-09-16 14:43:40 -0300

    nvim plugin undotree: remove vim.cmd section

--- nvim/.config/nvim/lua/plugins/vim-undotree.lua
+++ nvim/.config/nvim/lua/plugins/vim-undotree.lua
@@ -4,10 +4,8 @@ return {
                 { "<leader>u", "<CMD>UndotreeToggle<CR>", desc = "Toggle history bar" }
         },
         config = function()
-                vim.cmd([[
-                        let g:undotree_SplitWidth=float2nr(&columns * 0.27 + 0.5)
-                        let g:undotree_WindowLayout=4
-                        let g:undotree_SetFocusWhenToggle=1
-                ]])
+                vim.g.undotree_SplitWidth = math.floor(vim.o.columns * 0.27 + 0.5)
+                vim.g.undotree_WindowLayout = 4
+                vim.g.undotree_SetFocusWhenToggle = 1
         end
 }
