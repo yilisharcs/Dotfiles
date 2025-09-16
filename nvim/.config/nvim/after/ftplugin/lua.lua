3a1a94ef9111d1268cc3edcbc6bb9be518195832
yilisharcs <yilisharcs@gmail.com>
2025-09-16 17:13:48 -0300

    nvim ftplugin: fix colorcolumn on random files

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -1,6 +1,6 @@
 vim.bo.keywordprg = ":help"
 vim.bo.textwidth = 120
-vim.wo.colorcolumn = string.format("%d", vim.bo.textwidth)
+vim.wo[0][0].colorcolumn = string.format("%d", vim.bo.textwidth)
 
 vim.keymap.set("n", "<leader><leader>x", "<CMD>write | source %<CR>", { desc = "Source config file" })
 vim.keymap.set("n", "<leader><leader>c", ":lua <C-r><C-l><CR>", { desc = "Source config line" })
