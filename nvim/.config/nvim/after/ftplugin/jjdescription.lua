a08c2bb72f6bd63cb3d26d7445c3f273efac4fdc
yilisharcs <yilisharcs@gmail.com>
2025-09-16 22:36:29 -0300

    nvim ftplugin gitcommit and jjdescription: explicitly set keymap opts to { buffer = true } to avoid the keybinding leaking out

--- nvim/.config/nvim/after/ftplugin/jjdescription.lua
+++ nvim/.config/nvim/after/ftplugin/jjdescription.lua
@@ -1,5 +1,5 @@
 vim.cmd.startinsert()
 
 -- In honor of Neogit
-vim.keymap.set({ "n", "i" }, "<C-c><C-c>", "<CMD>wq<CR>")
-vim.keymap.set({ "n", "i" }, "<C-c><C-k>", "<CMD>q!<CR>")
+vim.keymap.set({ "n", "i" }, "<C-c><C-c>", "<CMD>wq<CR>", { buffer = true })
+vim.keymap.set({ "n", "i" }, "<C-c><C-k>", "<CMD>q!<CR>", { buffer = true })
