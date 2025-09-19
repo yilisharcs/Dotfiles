425dc4f400654ade1fca18d5428f77f8624d54f5
yilisharcs <yilisharcs@gmail.com>
2025-09-18 21:05:30 -0300

    nvim plugin hex: loaded a changelog.*.gz as binary one too many times

--- nvim/.config/nvim/lua/plugins/hexxd.lua
+++ nvim/.config/nvim/lua/plugins/hexxd.lua
@@ -1,5 +1,16 @@
 return {
         "RaafatTurki/hex.nvim",
-        event = "BufReadPre",
-        opts = {}
+        keys = {
+                { "<leader>y", "<CMD>HexToggle<CR>", desc = "Toggle hex dump" },
+        },
+        opts = {
+                -- Not too fond of this plugin trying to read files that
+                -- are NOT binary or are already read by other plugins
+                is_file_binary_pre_read = function()
+                        return false
+                end,
+                is_file_binary_post_read = function()
+                        return false
+                end,
+        }
 }
