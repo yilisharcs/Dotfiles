5f12c884a49dea3c2d395b4829954a08b2923da5
yilisharcs <yilisharcs@gmail.com>
2025-10-02 00:18:13 -0300

    nvim: My WIP plugin doesn't have a setup function anymore

--- neovim-config/.config/nvim/lua/plugins/wikibrowse.lua
+++ neovim-config/.config/nvim/lua/plugins/wikibrowse.lua
@@ -1,22 +1,19 @@
 return {
         "yilisharcs/wikibrowse.nvim",
         dev = true,
-        lazy = false,
-        cmd = "Wikibrowse",
-        keys = {
-                { "<leader>e", "<CMD>Wikibrowse pizza<CR>" },
-        },
-        init = function()
-                vim.keymap.set("ca", "wiki", function()
-                        if vim.fn.getcmdtype() == ":" then
-                                local cmd = vim.fn.getcmdline()
-                                if cmd:match("^wiki") then
-                                        return "Wikibrowse"
-                                else
-                                        return "wiki"
-                                end
-                        end
-                end, { expr = true })
-        end,
-        opts = true
+        config = function()
+                vim.g.wikibrowse = {
+                        -- lang = "pt"
+                }
+                -- vim.keymap.set("ca", "wiki", function()
+                --         if vim.fn.getcmdtype() == ":" then
+                --                 local cmd = vim.fn.getcmdline()
+                --                 if cmd:match("^wiki") then
+                --                         return "Wikibrowse"
+                --                 else
+                --                         return "wiki"
+                --                 end
+                --         end
+                -- end, { expr = true })
+        end
 }
