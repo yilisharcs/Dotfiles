cd8da37b89c61df0a65a708ad122d8ece9b976fd
yilisharcs <yilisharcs@gmail.com>
2025-10-10 01:36:20 -0300

    nvim plugin wikibrowse dev: custom commands

--- neovim-config/.config/nvim/lua/plugins/wikibrowse.lua
+++ neovim-config/.config/nvim/lua/plugins/wikibrowse.lua
@@ -6,15 +6,22 @@ return {
                         -- lang = "pt",
                         hello = "world"
                 }
-                -- vim.keymap.set("ca", "wiki", function()
-                --         if vim.fn.getcmdtype() == ":" then
-                --                 local cmd = vim.fn.getcmdline()
-                --                 if cmd:match("^wiki") then
-                --                         return "Wikibrowse"
-                --                 else
-                --                         return "wiki"
-                --                 end
-                --         end
-                -- end, { expr = true })
+
+                vim.api.nvim_create_user_command("Reload", function()
+                        vim.cmd("write")
+                        vim.cmd("restart")
+                end, {})
+                vim.keymap.set("n", "<leader>e", "<CMD>Wikibrowse pizza<CR>")
+                vim.keymap.set("n", "<leader>w", "<CMD>Reload<CR>")
+                vim.keymap.set("ca", "wiki", function()
+                        if vim.fn.getcmdtype() == ":" then
+                                local cmd = vim.fn.getcmdline()
+                                if cmd:match("^wiki") then
+                                        return "Wikibrowse"
+                                else
+                                        return "wiki"
+                                end
+                        end
+                end, { expr = true })
         end
 }
