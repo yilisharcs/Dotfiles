e49f7654b28911fed0da6c669814ee09a1288bef
yilisharcs <yilisharcs@gmail.com>
2025-10-10 12:18:06 -0300

    nvim queries: lua injection query

---
+++ neovim-config/.config/nvim/queries/lua/injections.scm
@@ -0,0 +1,10 @@
+(assignment_statement
+  (variable_list
+    name: (identifier) @_var
+    (#match? @_var "(query|parser_query|treesitter_query)"))
+  (expression_list
+    (string
+      content: (string_content) @injection.content)
+    ) @_str
+  (#lua-match? @_str "^%[%[")
+  (#set! injection.language "query"))
