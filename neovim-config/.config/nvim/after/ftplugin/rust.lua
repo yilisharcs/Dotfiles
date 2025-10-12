79a46acad57e22dd385a538a7e23a3fd5b68ab8d
yilisharcs <yilisharcs@gmail.com>
2025-10-12 15:53:32 -0300

    nvim shell: Nushell doesn't grok vi

--- neovim-config/.config/nvim/after/ftplugin/rust.lua
+++ neovim-config/.config/nvim/after/ftplugin/rust.lua
@@ -22,28 +22,3 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
                 vim.keymap.set("n", "-", "6s> ", { remap = true, buffer = true })
         end
 })
-
--- Although `let g:rustfmt_autosave = 1` exists,
--- running RustFmt directly populates the loclist
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-        group = vim.api.nvim_create_augroup("Rust_Auto_Fmt", { clear = true }),
-        pattern = "*.rs",
-        callback = function()
-                -- RustFmt expects a POSIX compliant shell with shellcmdflag
-                -- option set accordingly. Nushell is not POSIX compliant.
-                if vim.o.shell ~= "/usr/bin/bash" then
-                        local shell = vim.o.shell
-                        local shellcmdflag = vim.o.shellcmdflag
-
-                        vim.o.shell = vim.fn.exepath("bash")
-                        vim.o.shellcmdflag = "-c"
-
-                        vim.cmd("silent! RustFmt")
-
-                        vim.o.shell = shell
-                        vim.o.shellcmdflag = shellcmdflag
-                        return
-                end
-                vim.cmd("silent! RustFmt")
-        end
-})
