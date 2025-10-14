aa095d9ecf37586fa246bf08fd072f30f20e8f2d
yilisharcs <yilisharcs@gmail.com>
2025-10-14 00:32:31 -0300

    nvim plugin toggleterm: gemini term

--- neovim-config/.config/nvim/lua/plugins/toggleterm.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/toggleterm.nvim.lua
@@ -21,21 +21,29 @@ return {
 
                 local Terminal = require("toggleterm.terminal").Terminal
 
+                local gemini = Terminal:new({
+                        cmd = "tmux new -A -s gemini 'gemini'",
+                        direction = "vertical",
+                        display_name = "GEMINI MUX",
+                        count = 2
+                })
+                vim.keymap.set("n", "<leader><F2>", function() gemini:toggle() end)
+
                 local gh_dash = Terminal:new({
                         cmd = "gh dash",
                         dir = "~",
                         direction = "float",
                         display_name = "GITHUB DASHBOARD",
-                        count = 2
+                        count = 3
                 })
-                vim.keymap.set("n", "<leader><F2>", function() gh_dash:toggle() end)
+                vim.keymap.set("n", "<leader><F3>", function() gh_dash:toggle() end)
 
                 local btop = Terminal:new({
                         cmd = "btop",
                         direction = "float",
                         display_name = "RESOURCE MONITOR",
-                        count = 3
+                        count = 4
                 })
-                vim.keymap.set("n", "<leader><F3>", function() btop:toggle() end)
+                vim.keymap.set("n", "<leader><F4>", function() btop:toggle() end)
         end
 }
