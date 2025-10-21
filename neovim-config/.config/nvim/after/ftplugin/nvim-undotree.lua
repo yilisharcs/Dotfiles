218447cd15892cf493eabb3012c8c5d91f593149
yilisharcs <yilisharcs@gmail.com>
2025-10-21 18:55:02 -0300

    nvim undotree: cursorline

--- neovim-config/.config/nvim/after/ftplugin/nvim-undotree.lua
+++ neovim-config/.config/nvim/after/ftplugin/nvim-undotree.lua
@@ -1,5 +1,7 @@
 vim.wo[0][0].foldenable = false
 vim.wo[0][0].number = false
 vim.wo[0][0].relativenumber = false
+vim.wo[0][0].cursorlineopt = "line"
+vim.wo[0][0].winhighlight = "CursorLine:Folded"
 
 vim.keymap.set("n", "q", "<CMD>Undotree<CR>", { buffer = true })
