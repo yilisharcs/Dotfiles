832976af34561d80830232e22caa592418a6346d
yilisharcs <yilisharcs@gmail.com>
2025-10-24 00:35:19 -0300

    nvim colors: rewrite tricky in lua

--- neovim-config/.config/nvim/pack/3rd/start/conf/plugin/vim-sneak.lua
+++ neovim-config/.config/nvim/pack/3rd/start/conf/plugin/vim-sneak.lua
@@ -16,3 +16,23 @@ vim.keymap.set({ "n", "x", "o" }, "t", "<Plug>Sneak_t")
 vim.keymap.set({ "n", "x", "o" }, "T", "<Plug>Sneak_T")
 
 vim.g["sneak#use_ic_scs"] = 1
+
+local group = vim.api.nvim_create_augroup("SneakInsertMode", { clear = true })
+vim.api.nvim_create_autocmd("InsertEnter", {
+        group = group,
+        callback = function()
+                vim.api.nvim_set_hl(0, "Sneak", { link = "SneakHide", force = true })
+        end
+})
+vim.api.nvim_create_autocmd({ "ColorScheme", "InsertLeave" }, {
+        group = group,
+        callback = function()
+                vim.api.nvim_set_hl(0, "Sneak", { link = "SneakShow", force = true })
+        end
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+        group = group,
+        callback = function()
+                vim.api.nvim_set_hl(0, "SneakScope", { bg = "#060010" })
+        end
+})
