cafe37a2963d4e202639429e4ec18db9ae2012eb
yilisharcs <yilisharcs@gmail.com>
2025-10-24 03:05:06 -0300

    nvim vim.pack: disable PackChanged autocommands

--- neovim-config/.config/nvim/pack/3rd/start/conf/plugin/mason.nvim.lua
+++ neovim-config/.config/nvim/pack/3rd/start/conf/plugin/mason.nvim.lua
@@ -2,39 +2,38 @@ vim.pack.add({
         "https://github.com/williamboman/mason.nvim",
 })
 
-local group = vim.api.nvim_create_augroup("Vimpack_Mason", { clear = true })
-
-vim.api.nvim_create_autocmd("PackChanged", {
-        desc = "Update Mason binaries",
-        group = group,
-        callback = function(event)
-                if vim.g.did_vimpack_mason == 1 then return end
-                vim.g.did_vimpack_mason = 1
-                if event.data.kind == "update" or event.data.kind == "install" then
-                        local cmd = table.concat({
-                                ":MasonInstall",
-                                --
-                                "clangd",
-                                "lua-language-server",
-                                "tinymist",
-                                "vim-language-server",
-                        }, " ")
-                        ---@diagnostic disable-next-line: param-type-mismatch
-                        local ok = pcall(vim.cmd, cmd)
-                        if ok then
-                                vim.notify(
-                                        "MasonInstall completed successfully!",
-                                        vim.log.levels.INFO,
-                                        { title = "vim.pack" })
-                        else
-                                vim.notify(
-                                        "MasonInstall command not available yet, skipping",
-                                        vim.log.levels.WARN,
-                                        { title = "vim.pack" })
-                        end
-                end
-        end
-})
+-- local group = vim.api.nvim_create_augroup("Vimpack_Mason", { clear = true })
+-- vim.api.nvim_create_autocmd("PackChanged", {
+--         desc = "Update Mason binaries",
+--         group = group,
+--         callback = function(event)
+--                 if vim.g.did_vimpack_mason == 1 then return end
+--                 vim.g.did_vimpack_mason = 1
+--                 if event.data.kind == "update" or event.data.kind == "install" then
+--                         local cmd = table.concat({
+--                                 ":MasonInstall",
+--                                 --
+--                                 "clangd",
+--                                 "lua-language-server",
+--                                 "tinymist",
+--                                 "vim-language-server",
+--                         }, " ")
+--                         ---@diagnostic disable-next-line: param-type-mismatch
+--                         local ok = pcall(vim.cmd, cmd)
+--                         if ok then
+--                                 vim.notify(
+--                                         "MasonInstall completed successfully!",
+--                                         vim.log.levels.INFO,
+--                                         { title = "vim.pack" })
+--                         else
+--                                 vim.notify(
+--                                         "MasonInstall command not available yet, skipping",
+--                                         vim.log.levels.WARN,
+--                                         { title = "vim.pack" })
+--                         end
+--                 end
+--         end
+-- })
 
 require("mason").setup({
         ui = {
