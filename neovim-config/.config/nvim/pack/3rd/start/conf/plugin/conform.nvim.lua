211f07ea724787eb7f9c2472336c3899f0370521
yilisharcs <yilisharcs@gmail.com>
2025-10-24 02:35:23 -0300

    nvim plugin conform.nvim

---
+++ neovim-config/.config/nvim/pack/3rd/start/conf/plugin/conform.nvim.lua
@@ -0,0 +1,14 @@
+vim.pack.add({
+        "https://github.com/stevearc/conform.nvim",
+})
+
+require("conform").setup({
+        formatters_by_ft = {
+                lua = { "stylua" },
+                rust = { "rustfmt", lsp_format = "fallback" },
+        },
+        format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+        },
+})
