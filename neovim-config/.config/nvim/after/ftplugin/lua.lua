b0c230dcd0328ce786185b7ea738e467572ec301
yilisharcs <yilisharcs@gmail.com>
2025-10-25 01:49:37 -0300

    nvim ftplugin lua, vim: :. source

--- neovim-config/.config/nvim/after/ftplugin/lua.lua
+++ neovim-config/.config/nvim/after/ftplugin/lua.lua
@@ -2,4 +2,4 @@ vim.bo.keywordprg = ":help"
 vim.wo[0][0].colorcolumn = "120"
 
 vim.keymap.set("n", "<leader><leader>x", "<CMD>write | source %<CR>", { desc = "Source config file" })
-vim.keymap.set("n", "<leader><leader>c", ":lua <C-r><C-l><CR>", { desc = "Source config line" })
+vim.keymap.set("n", "<leader><leader>c", ":.lua<CR>", { desc = "Source config line" })
