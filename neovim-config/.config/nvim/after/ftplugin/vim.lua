3549cf53ab3ff91c3606600349568e536f5b9fb6
yilisharcs <yilisharcs@gmail.com>
2025-10-25 01:49:37 -0300

    nvim ftplugin lua, vim: :. source

--- neovim-config/.config/nvim/after/ftplugin/vim.lua
+++ neovim-config/.config/nvim/after/ftplugin/vim.lua
@@ -3,5 +3,5 @@ vim.opt_local.iskeyword:remove({ "-" })
 vim.bo.commentstring = '" %s'
 
 vim.keymap.set("n", "<leader><leader>x", "<CMD>write | source %<CR>", { desc = "Source config file" })
-vim.keymap.set("n", "<leader><leader>c", ":<C-r><C-l><CR>", { desc = "Source config line" })
+vim.keymap.set("n", "<leader><leader>c", ":.<CR>", { desc = "Source config line" })
 vim.keymap.set("x", "<leader><leader>c", '"zy:<C-r>z<CR><ESC>', { desc = "Source config line" })
