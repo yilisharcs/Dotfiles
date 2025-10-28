2562bb6f7179bd31b7b9f0a914e84fa5793ade6a
yilisharcs <yilisharcs@gmail.com>
2025-10-28 13:28:39 -0300

    nvim plugin mini.git: blame, jj

--- neovim-config/.config/nvim/pack/3rd/start/conf/plugin/mini.git.lua
+++ neovim-config/.config/nvim/pack/3rd/start/conf/plugin/mini.git.lua
@@ -1,6 +1,8 @@
+local executable = "git"
+
 require("mini.git").setup({
         job = {
-                git_executable = "git",
+                git_executable = executable,
         },
 })
 
@@ -14,11 +16,66 @@ vim.keymap.set("ca", "git", function()
                 end
         end
 end, { expr = true })
+vim.keymap.set("ca", "jj", function()
+        if vim.fn.getcmdtype() == ":" then
+                local cmd = vim.fn.getcmdline()
+                if cmd:match("^jj") then
+                        return "Git"
+                else
+                        return "jj"
+                end
+        end
+end, { expr = true })
 
-vim.keymap.set("n", "<leader>gb", "<CMD>vert Git blame -s -- %<CR><CMD>vert res 15<CR><C-w>w", { desc = "Git blame" })
-vim.keymap.set(
-        "n",
-        "<leader>gd",
-        "<CMD>diffthis<CR><CMD>vert Git show HEAD:%<CR><CMD>difft<CR><C-w>w",
-        { desc = "Diff current file" }
-)
+if executable == "git" then
+        vim.keymap.set(
+                "n",
+                "<leader>gd",
+                "<CMD>diffthis<CR><CMD>vert Git show HEAD:%<CR><CMD>difft<CR><C-w>w",
+                { desc = "Diff current file" }
+        )
+
+        vim.keymap.set(
+                "n",
+                "<leader>gb",
+                "mzgg<CMD>vert Git blame -- %<CR><C-w>l<CMD>set cursorbind<CR><CMD>set scrollbind<CR>`z",
+                { desc = "Git blame" }
+        )
+
+        local ns = vim.api.nvim_create_namespace("mini_git_blame")
+        local group = vim.api.nvim_create_augroup("MiniGitBlame", { clear = true })
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+                desc = "Format git blame",
+                pattern = "git",
+                group = group,
+                callback = function()
+                        local name = vim.api.nvim_buf_get_name(0)
+                        if not name:match("^minigit://.*/git blame") then return end
+
+                        local line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+                        local match = line:find("[+-]%d%d%d%d")
+                        vim.cmd.resize({ match + 5, mods = { vertical = true } })
+
+                        vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+                        local last_line = vim.api.nvim_buf_line_count(0) - 1
+                        for lnum = 0, last_line do
+                                vim.api.nvim_buf_set_extmark(0, ns, lnum, match + 4, {
+                                        virt_text = { { ")", "Normal" } },
+                                        virt_text_pos = "overlay",
+                                })
+                        end
+
+                        vim.api.nvim_set_option_value("cursorbind", true, { scope = "local" })
+                        vim.api.nvim_set_option_value("scrollbind", true, { scope = "local" })
+                        vim.api.nvim_set_option_value("winfixwidth", true, { scope = "local" })
+                        vim.api.nvim_set_option_value("winfixbuf", true, { scope = "local" })
+                        vim.api.nvim_set_option_value("number", false, { scope = "local" })
+                        vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
+                        vim.api.nvim_set_option_value("signcolumn", "no", { scope = "local" })
+                        vim.api.nvim_set_option_value("foldcolumn", "0", { scope = "local" })
+                        vim.api.nvim_set_option_value("statuscolumn", "", { scope = "local" })
+                end,
+        })
+elseif executable == "jj" then
+        -- Pending
+end
