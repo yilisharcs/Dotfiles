80089a11178c0a757c7a47b9097c014cb54425df
yilisharcs <yilisharcs@gmail.com>
2025-10-27 23:57:52 -0300

    nvim plugin diffview: split from neogit

--- neovim-config/.config/nvim/pack/3rd/start/conf/plugin/neogit.lua
+++ neovim-config/.config/nvim/pack/3rd/start/conf/plugin/neogit.lua
@@ -29,39 +29,3 @@ require("neogit").setup({
 })
 
 vim.keymap.set("n", "<leader>i", "<CMD>Neogit<CR>", { desc = "[Git] Open status tab" })
-
----
-
-local actions = require("diffview.actions")
-require("diffview").setup({
-        -- diff_binaries = true,
-        view = {
-                default = {
-                        layout = "diff2_vertical",
-                },
-        },
-        file_panel = {
-                win_config = {
-                        position = "right",
-                        width = math.floor(vim.o.columns * 0.33 + 0.5),
-                },
-        },
-        keymaps = {
-                view = {
-                        { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
-                        { "n", "<C-p>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
-                },
-                file_panel = {
-                        { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
-                        { "n", "<C-p>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
-                        { "n", "<C-k>", actions.scroll_view(-0.25), { desc = "Scroll the view up" } },
-                        { "n", "<C-j>", actions.scroll_view(0.25), { desc = "Scroll the view down" } },
-                },
-                file_history_panel = {
-                        { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
-                        { "n", "<C-p>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
-                        { "n", "<C-k>", actions.scroll_view(-0.25), { desc = "Scroll the view up" } },
-                        { "n", "<C-j>", actions.scroll_view(0.25), { desc = "Scroll the view down" } },
-                },
-        },
-})
