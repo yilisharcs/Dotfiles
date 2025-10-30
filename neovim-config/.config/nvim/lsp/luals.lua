d24864a69cea985f68725a84f2e28411fb8be79b
yilisharcs <yilisharcs@gmail.com>
2025-10-30 07:04:50 -0300

    nvim: global P

--- neovim-config/.config/nvim/lsp/luals.lua
+++ neovim-config/.config/nvim/lsp/luals.lua
@@ -22,6 +22,7 @@ return {
                                 globals = {
                                         "vim",
                                         "Snacks",
+                                        "P",
                                 },
                                 unusedLocalExclude = {
                                         "_*",
