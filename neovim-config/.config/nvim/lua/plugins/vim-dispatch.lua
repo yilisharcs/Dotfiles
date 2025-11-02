89afda543c7389e4898d6d121e64597cf14f7e2d
yilisharcs <yilisharcs@gmail.com>
2025-11-02 14:15:27 -0300

    nvim keymaps: remove abbrev duplication

--- neovim-config/.config/nvim/lua/plugins/vim-dispatch.lua
+++ neovim-config/.config/nvim/lua/plugins/vim-dispatch.lua
@@ -1,26 +1,9 @@
 return {
         "https://github.com/tpope/vim-dispatch",
         event = "CmdlineEnter",
-        init = function()
-                vim.keymap.set("ca", "make", function()
-                        if vim.fn.getcmdtype() == ":" then
-                                local cmd = vim.fn.getcmdline()
-                                if cmd:match("^make") then
-                                        return "Make"
-                                else
-                                        return "make"
-                                end
-                        end
-                end, { expr = true })
-                vim.keymap.set("ca", "mask", function()
-                        if vim.fn.getcmdtype() == ":" then
-                                local cmd = vim.fn.getcmdline()
-                                if cmd:match("^mask") then
-                                        return "Make"
-                                else
-                                        return "mask"
-                                end
-                        end
-                end, { expr = true })
+        config = function()
+                require("utils.cabbrev")({
+                        ["Make"] = { "make", "mask" },
+                })
         end,
 }
