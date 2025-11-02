b4e9c94a58f8f5bfda28272d0bb7662b615bf9cb
yilisharcs <yilisharcs@gmail.com>
2025-11-02 14:15:18 -0300

    nvim plugin vim-abolish

---
+++ neovim-config/.config/nvim/lua/plugins/vim-abolish.lua
@@ -0,0 +1,8 @@
+return {
+        "https://github.com/tpope/vim-abolish",
+        config = function()
+                vim.cmd([[
+                        " Abolish foo bar
+                ]])
+        end,
+}
