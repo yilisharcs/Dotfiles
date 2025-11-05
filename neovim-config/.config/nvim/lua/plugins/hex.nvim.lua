d10b9e1c11e74e20fbaa9d48ef7cd7d5bc8da129
yilisharcs <yilisharcs@gmail.com>
2025-11-05 05:43:08 -0300

    nvim plugin hex.nvim: dev time is over

--- neovim-config/.config/nvim/lua/plugins/hex.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/hex.nvim.lua
@@ -1,45 +1,9 @@
 return {
         "https://github.com/RaafatTurki/hex.nvim",
-        dev = true,
         lazy = false,
-        keys = {
-                {
-                        "<leader>b",
-                        "<CMD>Hex toggle<CR>",
-                        desc = "Toggle hex dump",
-                },
-        },
         init = function()
-                vim.g.hex = {
-                        keymaps = false,
-                        prettify = {
-                                unicode = false,
-                        },
-                        -- Not too fond of this plugin trying to read files that
-                        -- are NOT binary or are already read by other plugins
-                        -- checkbin_pre = function()
-                        --         return false
-                        -- end,
-                        -- checkbin_post = function()
-                        --         return false
-                        -- end,
-                }
-
-                vim.keymap.set(
-                        "n",
-                        "u",
-                        "<Plug>(HexUndo)",
-                        { desc = "Undo one change" }
-                )
-                vim.keymap.set(
-                        "n",
-                        "U",
-                        "<Plug>(HexRedo)",
-                        { desc = "Redo one change which was undone" }
-                )
-
                 require("utils.cabbrev")({
-                        ["Hex"] = { "hex" },
+                        ["HexToggle"] = { "hex" },
                 })
         end,
 }
