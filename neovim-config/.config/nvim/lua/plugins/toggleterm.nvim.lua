2795c3069301a4f5981ca2a68719ec9b0ff41def
yilisharcs <yilisharcs@gmail.com>
2025-11-08 19:11:53 -0300

    nvim plugin toggleterm.nvim: fix size to return integers

--- neovim-config/.config/nvim/lua/plugins/toggleterm.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/toggleterm.nvim.lua
@@ -8,13 +8,13 @@ return {
                         or vim.o.shell,
                 float_opts = {
                         border = "rounded",
-                        height = vim.o.lines * 0.8,
+                        height = math.floor(vim.o.lines * 0.8),
                 },
                 size = function(term)
                         if term.direction == "horizontal" then
-                                return vim.o.lines * 0.4
+                                return math.floor(vim.o.lines * 0.4)
                         elseif term.direction == "vertical" then
-                                return vim.o.columns * 0.5
+                                return math.floor(vim.o.columns * 0.5)
                         end
                 end,
         },
