845f9e62ac51c02b032413f553926982774e3098
yilisharcs <yilisharcs@gmail.com>
2025-11-14 17:01:07 -0300

    nvim ftplugin rust: i prefer my own options

--- neovim-config/.config/nvim/after/ftplugin/rust.lua
+++ neovim-config/.config/nvim/after/ftplugin/rust.lua
@@ -1,6 +1,8 @@
 vim.b.editorconfig = false
+vim.g.rust_recommended_style = false
 
 vim.bo.commentstring = "// %s"
+vim.bo.textwidth = 99
 
 require("utils.cabbrev")({
         ["Cargo"] = { "cargo" },
