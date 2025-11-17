e98b437208897bccab50ef0e58f6e2a26472365e
yilisharcs <yilisharcs@gmail.com>
2025-11-17 19:59:26 -0300

    nvim ftplugin rust: use editorconfig
    Problem: Annoying mismatch between line indent if launched for the first
    time
    
    Solution: editorconfig my beloved

--- neovim-config/.config/nvim/after/ftplugin/rust.lua
+++ neovim-config/.config/nvim/after/ftplugin/rust.lua
@@ -1,8 +1,6 @@
-vim.b.editorconfig = false
 vim.g.rust_recommended_style = false
 
 vim.bo.commentstring = "// %s"
-vim.bo.textwidth = 99
 vim.wo[0][0].colorcolumn = "100"
 
 require("utils.cabbrev")({
