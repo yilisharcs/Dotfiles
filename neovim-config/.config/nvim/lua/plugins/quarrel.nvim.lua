be8ac1ddc90ef2f2c15735e560444a94c6634152
yilisharcs <yilisharcs@gmail.com>
2025-11-22 06:26:19 -0300

    nvim plugin quarrel.nvim: don't trigger if using shell editor

--- neovim-config/.config/nvim/lua/plugins/quarrel.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/quarrel.nvim.lua
@@ -1,6 +1,7 @@
 return {
         "https://github.com/yilisharcs/quarrel.nvim",
         dev = true,
+        cond = not vim.g.shell_editor == true,
         init = function()
                 vim.g.quarrel = {
                         keymaps = true,
