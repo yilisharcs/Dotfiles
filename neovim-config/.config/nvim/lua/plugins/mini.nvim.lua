69edbadd7b209c62db225edf44b59c4b7632ea97
yilisharcs <yilisharcs@gmail.com>
2025-12-03 07:48:57 -0300

    nvim plugin mini.pairs: disable

--- neovim-config/.config/nvim/lua/plugins/mini.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/mini.nvim.lua
@@ -271,48 +271,6 @@ return {
                 })
                 -- }}}
 
-                -- mini.pairs {{{
-                require("mini.pairs").setup({
-                        modes = {
-                                insert = true,
-                                command = false,
-                                terminal = false,
-                        },
-                        mappings = {
-                                --stylua: ignore start
-                                ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\].", register = { bs = true, cr = true } },
-                                ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\].", register = { bs = true, cr = true } },
-                                ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\].", register = { bs = true, cr = true } },
-                                ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^ \\].", register = { bs = true } },
-                                [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
-                                ["|"] = { action = "closeopen", pair = "||", neigh_pattern = "[\\{\\][}]", register = { bs = true } }, -- closures
-                                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a&<\\][^>]", register = { cr = false }, }, -- lifetimes
-                                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%a\\].", register = { cr = false } },
-                                --stylua: ignore end
-                        },
-                })
-
-                local map_bs = function(lhs, rhs)
-                        vim.keymap.set(
-                                "i",
-                                lhs,
-                                rhs,
-                                { expr = true, replace_keycodes = false }
-                        )
-                end
-
-                map_bs("<C-h>", "v:lua.MiniPairs.bs()")
-                map_bs("<C-w>", "v:lua.MiniPairs.bs('\23')")
-                map_bs("<C-u>", "v:lua.MiniPairs.bs('\21')")
-
-                vim.cmd([[
-                        augroup Cmdlinewin_No_Pairs
-                                au!
-                                au CmdwinEnter * lua vim.b.minipairs_disable = true
-                        augroup END
-                ]])
-                -- }}}
-
                 -- mini.sessions {{{
                 require("mini.sessions").setup({
                         force = {
