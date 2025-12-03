50e0d592562b8e4d412765c4c4eda6c2709236ff
yilisharcs <yilisharcs@gmail.com>
2025-12-03 18:06:36 -0300

    nvim plugin cme.nvim: now with Recompile command

--- neovim-config/.config/nvim/lua/plugins/cme.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/cme.nvim.lua
@@ -8,9 +8,11 @@ return {
 
                 vim.keymap.set("n", "<leader>c", ":Compile ")
                 vim.keymap.set("n", "<leader><S-c>", "<CMD>Compile<CR>")
+                vim.keymap.set("n", "<leader>r", ":Recompile ")
 
                 require("utils.cabbrev")({
                         ["Compile"] = { "c", "C", "compile" },
+                        ["Recompile"] = { "r", "R", "recompile" },
                         ["Compile fd --strip-cwd-prefix=never"] = { "find" },
                         ["Compile git"] = { "git" },
                         ["Compile grep"] = { "grep" },
