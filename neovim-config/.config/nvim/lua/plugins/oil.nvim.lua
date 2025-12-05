f53737cf6479949555549de2fea0f665364c9cf7
yilisharcs <yilisharcs@gmail.com>
2025-12-05 01:17:15 -0300

    nvim plugin oil.nvim, yazi.nvim: hello oil, bye yazi

---
+++ neovim-config/.config/nvim/lua/plugins/oil.nvim.lua
@@ -0,0 +1,51 @@
+return {
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        lazy = false,
+        keys = {
+                { "-", "<CMD>Oil<CR>", desc = "Open file explorer" },
+        },
+        opts = {
+                default_file_explorer = true,
+                columns = {
+                        "icon",
+                        "permissions",
+                        "size",
+                        "mtime",
+                },
+                win_options = {
+                        signcolumn = "yes:1",
+                },
+                delete_to_trash = true,
+                skip_confirm_for_simple_edits = true,
+                prompt_save_on_select_new_entry = true,
+                lsp_file_methods = {
+                        enabled = false,
+                },
+                constrain_cursor = "editable",
+                view_options = {
+                        show_hidden = true,
+                },
+                watch_for_changes = true,
+                keymaps = {
+                        ["<leader>fl"] = {
+                                function()
+                                        require("fzf-lua").files({
+                                                cwd = require("oil").get_current_dir(),
+                                        })
+                                end,
+                                mode = "n",
+                                nowait = true,
+                                desc = "Find files from current directory",
+                        },
+                        ["<leader>;"] = {
+                                "actions.open_cmdline",
+                                opts = {
+                                        shorten_path = true,
+                                        modify = ":h",
+                                },
+                                desc = "Enter cmdline with the current directory as an argument",
+                        },
+                },
+        },
+}
