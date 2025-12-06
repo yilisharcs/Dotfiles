86af5aabb2bb6363ee9c0f51c73ad6fa6d566afb
yilisharcs <yilisharcs@gmail.com>
2025-12-06 20:16:54 -0300

    nvim colors silverwine
    - tabline
    - title
    - quickfix
    - neogit
    - matchparen
    - delimiter

--- neovim-config/.config/nvim/after/ftplugin/qf.lua
+++ neovim-config/.config/nvim/after/ftplugin/qf.lua
@@ -2,11 +2,12 @@ vim.wo[0][0].list = false
 vim.wo[0][0].relativenumber = false
 vim.wo[0][0].signcolumn = "no"
 vim.wo[0][0].statuscolumn = ""
-vim.wo[0][0].winhighlight = "Normal:NormalNC"
 vim.wo[0][0].wrap = false
 
 local middle = math.floor(vim.o.lines * 0.5)
 vim.api.nvim_win_set_height(0, middle)
+
+vim.wo[0][0].winhighlight = "Normal:QuickFixBg"
 
 vim.fn.matchadd("DiagnosticError", [[^\zsE\ze|]])
 vim.fn.matchadd("DiagnosticWarn", [[^\zsW\ze|]])
