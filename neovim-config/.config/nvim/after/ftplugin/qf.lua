fecaa9ebab3d7663c2315d34f89b8c034e31d14b
yilisharcs <yilisharcs@gmail.com>
2025-12-07 02:51:16 -0300

    Revert "nvim ftplugin qf: auto resize to half of screen"
    This reverts commit 2a82e512cf7c1758920ead3c9c451a9eeed55c37.

--- neovim-config/.config/nvim/after/ftplugin/qf.lua
+++ neovim-config/.config/nvim/after/ftplugin/qf.lua
@@ -4,9 +4,6 @@ vim.wo[0][0].signcolumn = "no"
 vim.wo[0][0].statuscolumn = ""
 vim.wo[0][0].wrap = false
 
-local middle = math.floor(vim.o.lines * 0.5)
-vim.api.nvim_win_set_height(0, middle)
-
 vim.wo[0][0].winhighlight = "Normal:QuickFixBg"
 
 vim.fn.matchadd("DiagnosticError", [[^\zsE\ze|]])
