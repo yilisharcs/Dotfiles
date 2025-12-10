2016e4f7d486c49fc18b9feb9bf479788a731e16
yilisharcs <yilisharcs@gmail.com>
2025-12-10 13:45:31 -0300

    nvim plugin hex.nvim: forgot to pass opts table

--- neovim-config/.config/nvim/lua/plugins/hex.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/hex.nvim.lua
@@ -6,4 +6,14 @@ return {
                         ["HexToggle"] = { "hex" },
                 })
         end,
+        opts = {
+                -- Not too fond of this plugin trying to read files that
+                -- are NOT binary or are already read by other plugins
+                is_file_binary_pre_read = function()
+                        return false
+                end,
+                is_file_binary_post_read = function()
+                        return false
+                end,
+        },
 }
