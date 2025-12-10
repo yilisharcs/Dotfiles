ca80ef9f3fdb7502f7ed453fdbddafab66cd1ffe
yilisharcs <yilisharcs@gmail.com>
2025-12-10 13:41:51 -0300

    nvim plugin direnv.vim: enable silent load

--- neovim-config/.config/nvim/lua/plugins/direnv.vim.lua
+++ neovim-config/.config/nvim/lua/plugins/direnv.vim.lua
@@ -1,5 +1,8 @@
 return {
         "https://github.com/yilisharcs/direnv.vim",
         branch = "clean-ansi-codes",
+        init = function()
+                vim.g.direnv_silent_load = 1
+        end,
         -- dev = true,
 }
