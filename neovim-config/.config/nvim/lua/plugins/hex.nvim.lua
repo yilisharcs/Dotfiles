5e58f67816fe6c4fbcd0568755b12fe34aaa2f8e
yilisharcs <yilisharcs@gmail.com>
2025-12-10 14:18:44 -0300

    nvim plugin hex.nvim: using fork until further notice

--- neovim-config/.config/nvim/lua/plugins/hex.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/hex.nvim.lua
@@ -1,19 +1,36 @@
 return {
-        "https://github.com/RaafatTurki/hex.nvim",
+        "https://github.com/yilisharcs/hex.nvim",
+        -- branch = "undolevel",
+        dev = true,
         lazy = false,
+        keys = {
+                {
+                        "<leader>b",
+                        "<CMD>Hex toggle<CR>",
+                        desc = "Toggle hex dump",
+                },
+        },
         init = function()
+                vim.g.hex = {
+                        keymaps = false,
+                        prettify = {
+                                unicode = true,
+                        },
+                        -- Not too fond of this plugin trying to read files that
+                        -- are NOT binary or are already read by other plugins
+                        -- checkbin_pre = function()
+                        --         return false
+                        -- end,
+                        -- checkbin_post = function()
+                        --         return false
+                        -- end,
+                }
+
+                vim.keymap.set("n", "u", "<Plug>(HexUndo)", { desc = "Undo one change" })
+                vim.keymap.set("n", "U", "<Plug>(HexRedo)", { desc = "Redo one change" })
+
                 require("utils.cabbrev")({
-                        ["HexToggle"] = { "hex" },
+                        ["Hex"] = { "hex" },
                 })
         end,
-        opts = {
-                -- Not too fond of this plugin trying to read files that
-                -- are NOT binary or are already read by other plugins
-                is_file_binary_pre_read = function()
-                        return false
-                end,
-                is_file_binary_post_read = function()
-                        return false
-                end,
-        },
 }
