3dd89425e5c1228204fce927858824ff13363045
yilisharcs <yilisharcs@gmail.com>
2025-12-14 05:55:46 -0300

    nvim plugin conform.nvim: faster cargo fmt

--- neovim-config/.config/nvim/lua/plugins/conform.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/conform.nvim.lua
@@ -5,7 +5,11 @@ return {
                 formatters_by_ft = {
                         c = { "clang-format" },
                         lua = { "stylua" },
-                        rust = { "rustfmt", lsp_format = "fallback" },
+                        rust = {
+                                -- Rust shim is slower than providing the path manually
+                                "/home/yilisharcs/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rustfmt",
+                                lsp_format = "fallback",
+                        },
                 },
                 format_on_save = {
                         timeout_ms = 2000,
