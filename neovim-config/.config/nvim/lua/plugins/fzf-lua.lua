ab5baf94a980c8719e2820e8500b098bc5bff2f8
yilisharcs <yilisharcs@gmail.com>
2025-12-16 03:26:55 -0300

    nvim plugin fzf-lua: don't include pack in the project picker

--- neovim-config/.config/nvim/lua/plugins/fzf-lua.lua
+++ neovim-config/.config/nvim/lua/plugins/fzf-lua.lua
@@ -93,7 +93,6 @@ return {
                                 }
                                 local dirs = {
                                         "~/Projects/github.com/yilisharcs/",
-                                        "~/.config/nvim/pack/dev/opt/",
                                         "~/.local/share/nvim/lazy/",
                                         "~/.local/share/bob/nightly/share/nvim/runtime/",
                                         "~/Documents/",
