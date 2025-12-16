c612d35e6c41f3961544475f1ef050cef8c87a99
yilisharcs <yilisharcs@gmail.com>
2025-12-16 12:09:56 -0300

    nvim tafsk: toggle

--- neovim-config/.config/nvim/plugin/tafsk.lua
+++ neovim-config/.config/nvim/plugin/tafsk.lua
@@ -45,3 +45,24 @@ vim.keymap.set("n", "<C-]>", function()
                 end
         end
 end)
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+        desc = "Toggle tafsk status",
+        group = vim.api.nvim_create_augroup("Tafsk", { clear = true }),
+        pattern = "markdown",
+        callback = function()
+                vim.keymap.set("n", "<CR>", function()
+                        local row, _col = unpack(vim.api.nvim_win_get_cursor(0))
+                        local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+                        -- stylua: ignore
+                        if line:find("status: OPEN", 1, true) then
+                                vim.api.nvim_buf_set_lines(0, row - 1, row, false, { "status: CLOSED" })
+                        elseif line:find("status: CLOSED", 1, true) then
+                                vim.api.nvim_buf_set_lines(0, row - 1, row, false, { "status: OPEN" })
+                        else
+                                local key = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+                                vim.api.nvim_feedkeys(key, "n", false)
+                        end
+                end)
+        end,
+})
