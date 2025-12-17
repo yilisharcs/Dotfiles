3c3e1b4feb433249443020253617c9fc67a298cd
yilisharcs <yilisharcs@gmail.com>
2025-12-17 01:00:11 -0300

    nvim nosmartquotes: include exception for neovim

--- neovim-config/.config/nvim/plugin/nosmartquotes.lua
+++ neovim-config/.config/nvim/plugin/nosmartquotes.lua
@@ -2,12 +2,13 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         desc = 'Remove stupid "smart" quotes',
         group = vim.api.nvim_create_augroup("NoSmartQuotes", { clear = true }),
         callback = function()
-                local bufname = vim.api.nvim_buf_get_name(0)
-                if string.match(bufname, ".config/nvim/plugin/nosmartquotes.lua") == nil then
-                        local cursor = vim.api.nvim_win_get_cursor(0)
-                        vim.cmd('silent! keeppatterns %s/[“”]/"/ge')
-                        vim.cmd("silent! keeppatterns %s/[‘’]/'/ge")
-                        vim.api.nvim_win_set_cursor(0, cursor)
-                end
+                local name = vim.api.nvim_buf_get_name(0)
+                if not name:find(".config/nvim/plugin/nosmartquotes.lua", 1, true) then return end
+                if not name:find("Projects/github.com/neovim/neovim/", 1, true) then return end
+
+                local cursor = vim.api.nvim_win_get_cursor(0)
+                vim.cmd('silent! keeppatterns %s/[“”]/"/ge')
+                vim.cmd("silent! keeppatterns %s/[‘’]/'/ge")
+                vim.api.nvim_win_set_cursor(0, cursor)
         end,
 })
