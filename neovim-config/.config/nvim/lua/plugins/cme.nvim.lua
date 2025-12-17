f400343dd5f58c7c35068b3d5dfc100dcb91d1dd
yilisharcs <yilisharcs@gmail.com>
2025-12-17 16:24:17 -0300

    nvim ftplugin c: default compiler, clean up cme.nvim C efm table

--- neovim-config/.config/nvim/lua/plugins/cme.nvim.lua
+++ neovim-config/.config/nvim/lua/plugins/cme.nvim.lua
@@ -1,38 +1,5 @@
 local efm = {}
 
--- c {{{
-efm.c = table.concat({
-        "%*[^\"]\"%f\"%*\\D%l: %m",
-        "\"%f\"%*\\D%l: %m",
-        "%-Gg%\\?make[%*\\d]: *** [%f:%l:%m",
-        "%-Gg%\\?make: *** [%f:%l:%m",
-        "%-G%f:%l: (Each undeclared identifier is reported only once",
-        "%-G%f:%l: for each function it appears in.)",
-        "%-GIn file included from %f:%l:%c:",
-        "%-GIn file included from %f:%l:%c\\ ",
-        "%-GIn file included from %f:%l:%c",
-        "%-GIn file included from %f:%l",
-        "%-G%*[ ]from %f:%l:%c",
-        "%-G%*[ ]from %f:%l:",
-        "%-G%*[ ]from %f:%l\\ ",
-        "%-G%*[ ]from %f:%l",
-        -- Capture error type {{{
-        "%f:%l:%c: %t%*[^:]: %m",
-        -- }}}
-        "%f:%l:%c:%m",
-        "%f(%l):%m",
-        "%f:%l:%m",
-        "\"%f\"\\",
-        " line %l%*\\D%c%*[^ ] %m",
-        "%D%*\\a[%*\\d]: Entering directory %*[`']%f'",
-        "%X%*\\a[%*\\d]: Leaving directory %*[`']%f'",
-        "%D%*\\a: Entering directory %*[`']%f'",
-        "%X%*\\a: Leaving directory %*[`']%f'",
-        "%DMaking %*\\a in %f",
-        "%f|%l| %m",
-}, ",")
--- }}}
-
 -- rust {{{
 efm.rust = table.concat({
         "%-G",
@@ -78,9 +45,8 @@ return {
                 vim.g.cme = {
                         shell = "nu",
                         efm_rules = {
+                                ["buffer"] = { "mask" },
                                 [efm.rust] = { "cargo", "mask" },
-                                [efm.c] = { "make", "mask" },
-                                -- NOTE: zk/.local/bin/task
                                 [vim.o.grepformat] = { "task" },
                         },
                 }
