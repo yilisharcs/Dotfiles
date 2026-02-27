8098c8e95c7f2c97d97c160438c000b91b54e9fc
yilisharcs <yilisharcs@gmail.com>
2026-02-27 19:44:25 -0300

    wine: enable

--- modules/common/wine/default.nix
+++ modules/common/wine/default.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
     home-manager.sharedModules = [{
-        # home.packages = [
-        #     # pkgs.firejail                   # Namespace-based sandboxing tool for Linux
-        #     pkgs.winetricks                 # Easy way to work around problems in Wine
-        #     # NOTE: stable, full, wayland, waylandFull
-        #     # -- (DEPS: Fightcade, Sonic Mania, etc) Windows compatibility layer
-        #     pkgs.wineWow64Packages.stable # Support for both 32-bit and 64-bit Windows applications
-        # ];
+        home.packages = [
+            # pkgs.firejail                   # Namespace-based sandboxing tool for Linux
+            pkgs.winetricks                 # Easy way to work around problems in Wine
+            ## NOTE: stable, full, wayland, waylandFull
+            # -- (DEPS: Fightcade, Sonic Mania, etc) Windows compatibility layer
+            pkgs.wineWow64Packages.stable   # Support for both 32-bit and 64-bit Windows applications
+        ];
     }];
 }
