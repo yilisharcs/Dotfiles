a154863fd680bdaf11538f6b5b4f93031a28baaf
yilisharcs <yilisharcs@gmail.com>
2026-02-27 01:07:12 -0300

    Revert "nvim: nix nightly overlay"
    This reverts commit 638fd2cdb1af2f737ecf69dca54e4de5042af9be.
    
    Why? I'm not actually using any nightly feature; the experimental
    pager is not a priority, but what is is not recompiling the editor.

--- flake.nix
+++ flake.nix
@@ -33,11 +33,6 @@
             url = "github:nix-community/fenix";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-
-        neovim-nightly-overlay = {
-            url = "github:nix-community/neovim-nightly-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
     };
 
     outputs = inputs @ { self, nixpkgs, ... }: let
