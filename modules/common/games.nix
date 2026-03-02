97369aa4c0af41db27b01a75a3b7b58129e42088
yilisharcs <yilisharcs@gmail.com>
2026-03-02 15:15:34 -0300

    nix: improve fightcade package with home-manager

--- modules/common/games.nix
+++ modules/common/games.nix
@@ -1,11 +1,18 @@
-{ pkgs, ... }: {
-    home-manager.sharedModules = [{
+{ lib, pkgs, ... }: let
+    inherit (lib) disabled enabled;
+in {
+    home-manager.sharedModules = [({ config, ... }: {
+        programs.fightcade = enabled {
+            dataDir = "${config.home.homeDirectory}/Games/Fightcade";
+            autoDownloadROMs = true;
+            sf3TrainingMode = true;
+        };
+
         home.packages = [
-            pkgs.fightcade
             pkgs.mega-man-x8-16bit
             pkgs.sonic3air
             pkgs.sonic3air-beta
             pkgs.super-mario-63
         ];
-    }];
+    })];
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,9 +9,20 @@
                 (name: _: pkgs.callPackage (dir + "/${name}/package.nix") { })
                 (filterAttrs (name: value: value == "directory") (builtins.readDir dir));
 
+    loadModules = type: module:
+        let
+            dir = ./${type};
+            fileName = "${module}.nix";
+            dirs = filterAttrs (name: value: value == "directory") (builtins.readDir dir);
+            withModule = filterAttrs (name: _: builtins.pathExists (dir + "/${name}/${fileName}")) dirs;
+        in
+            mapAttrs (name: _: import (dir + "/${name}/${fileName}")) withModule;
+
     games  = loadPackages "games";
     # Add more categories as needed:
     tools  = loadPackages "tools";
     # themes = loadPackages "themes";
 
-in games // tools # // themes
+in games // tools // {
+    homeModules = loadModules "games" "home" // loadModules "tools" "home";
+}
