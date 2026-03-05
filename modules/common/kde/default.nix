4063de79bcae35663c30da012cb794e2be376802
yilisharcs <yilisharcs@gmail.com>
2026-03-05 03:57:13 -0300

    kde: notes... i've no clue why overrideConfig is so ass

--- modules/common/kde/default.nix
+++ modules/common/kde/default.nix
@@ -32,7 +32,7 @@ in {
 
         programs.plasma = enabled {
             immutableByDefault = true;
-            overrideConfig = true; # NOTE: occasionally breaks
+            # overrideConfig = true; # NOTE: occasionally breaks
             configFile = {
                 kded_device_automounterrc."Devices/\\/org\\/freedesktop\\/UDisks2\\/block_devices\\/sdb1" = {
                     EverMounted = true;
@@ -169,6 +169,11 @@ in {
             #     captureWindowUnderCursor = "Meta+Shift+S";
             # };
 
+            # TODO: figure out how to control this seemingly unexposed option:
+            #              right-click
+            #           -> desktop and wallpaper (home settings)
+            #           -> location
+            #           -> places panel item: $HOME
             panels = [{
                 location = "top";
                 floating = false;
