cbbdbbc82f1ef6379d9bc1b68c4fbd5d6a05d258
yilisharcs <yilisharcs@gmail.com>
2026-03-06 01:24:30 -0300

    nix: extra trinkers

--- modules/horse/default.nix
+++ modules/horse/default.nix
@@ -3,9 +3,7 @@
         home.packages = [
             pkgs.aseprite       # Animated sprite editor & pixel art tool
             pkgs.libreoffice    # Office productivity suite
-            ## TODO: home-configuration.nix:programs.obs-studio
-            # pkgs.obs-studio   # FOSS program for video recording and live streaming
-            # pkgs.picard
+            pkgs.picard         # MusicBrainz tagger
         ];
     }];
 }
