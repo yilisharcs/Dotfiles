8af7fcd312a75541874c8ec90903276df6f48925
yilisharcs <yilisharcs@gmail.com>
2026-03-16 10:40:56 -0300

    libreoffice: not just for horse

--- modules/horse/default.nix
+++ modules/horse/default.nix
@@ -2,7 +2,6 @@
     home-manager.sharedModules = [{
         home.packages = [
             pkgs.aseprite       # Animated sprite editor & pixel art tool
-            pkgs.libreoffice    # Office productivity suite
             pkgs.picard         # MusicBrainz tagger
         ];
     }];
