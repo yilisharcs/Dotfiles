cfc0c21a19b7793aafbf4d0697b466faadcedb16
yilisharcs <yilisharcs@gmail.com>
2026-05-02 09:33:05 -0300

    love: doesn't require a dedicated file yet

--- modules/common/love.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-    home-manager.sharedModules = [{
-        home.packages = [
-            pkgs.love              # Lua-based 2D game engine/scripting language
-        ];
-    }];
-}
