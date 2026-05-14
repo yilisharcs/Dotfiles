e40133345a22f4146024c904c7213acd9c82c497
yilisharcs <yilisharcs@gmail.com>
2026-05-13 23:21:16 -0300

    carapace: bridges for nushell

--- modules/common/shell/carapace.nix
+++ modules/common/shell/carapace.nix
@@ -1,8 +1,18 @@
-{ lib, ... }: let
+{ lib, pkgs, ... }: let
     inherit (lib) enabled;
 in {
     home-manager.sharedModules = [{
+        home.packages = [
+            pkgs.fish
+            pkgs.zsh
+            pkgs.inshellisense
+        ];
+
         # Multishell completion engine
         programs.carapace = enabled;
+
+        programs.nushell.environmentVariables = {
+            CARAPACE_BRIDGES = "zsh,fish,bash,inshellisense";
+        };
     }];
 }
