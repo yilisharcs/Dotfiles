f935a68339696f44bf26e099ec177ba23b7833a0
yilisharcs <yilisharcs@gmail.com>
2026-05-18 14:37:54 -0300

    qbittorrent: it occurs to me that i haven't used this in ages

--- modules/horse/qbittorrent/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-    home-manager.sharedModules = [{
-        ## TODO: configuration.nix:services.qbittorrent
-        home.packages = [ pkgs.qbittorrent ];
-
-        xdg.configFile."qBittorrent/themes/catppuccin-mocha.qbtheme".source = ./themes/catppuccin-mocha.qbtheme;
-    }];
-}
