e6cbf983adad7c5b8ee1a1cad5a14a07148cc533
yilisharcs <yilisharcs@gmail.com>
2026-05-22 15:05:30 -0300

    nvim: address rtp pollution

--- modules/common/neovim/default.nix
+++ modules/common/neovim/default.nix
@@ -2,13 +2,41 @@
     inherit (lib) enabled;
 
     # TODO: remove once `https://github.com/neovim/neovim/pull/36262` is merged
-    neovim = pkgs.neovim-unwrapped.overrideAttrs (old: {
+    neovim-unwrapped = pkgs.neovim-unwrapped.overrideAttrs (old: {
         patches = (old.patches or []) ++ [
             ./patch/0001-feat-undo-re-sync-undo-files-on-external-change.patch
             ./patch/0002-feat-undo-add-check-for-large-undo-files.patch
             ./patch/0003-docs-new-backup-behavior.patch
         ];
     });
+
+    # NOTE: these $XDG_ vars pollute the runtimepath, which can cause a
+    #       noticeable slowdown. TODO: consider upstreaming this.
+    neovim = (pkgs.symlinkJoin {
+        name = "neovim";
+        paths = [ neovim-unwrapped ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+            wrapProgram $out/bin/nvim \
+                --run '
+                    filter_xdg() (
+                        IFS=:
+                        new_dirs=""
+                        for d in $1; do
+                            [ -d "$d/nvim" ] && new_dirs="''${new_dirs:+$new_dirs:}$d"
+                        done
+                        echo "$new_dirs"
+                    )
+                    export XDG_DATA_DIRS=$(filter_xdg "$XDG_DATA_DIRS")
+                    export XDG_CONFIG_DIRS=$(filter_xdg "$XDG_CONFIG_DIRS")
+                '
+        '';
+    }) // {
+        meta = (neovim-unwrapped.meta or { }) // {
+            mainProgram = "nvim";
+            priority = (neovim-unwrapped.meta.priority or lib.meta.defaultPriority) - 1;
+        };
+    };
 in {
     home-manager.sharedModules = [{
         # Multilanguage implementation of ctags
