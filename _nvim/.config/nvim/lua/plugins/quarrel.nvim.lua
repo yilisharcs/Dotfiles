97686ec80432b2470e45a85d094edea62eb4d072
yilisharcs <yilisharcs@gmail.com>
2026-05-22 10:28:11 -0300

    nvim plugins quarrel.nvim: update

--- _nvim/.config/nvim/lua/plugins/quarrel.nvim.lua
+++ _nvim/.config/nvim/lua/plugins/quarrel.nvim.lua
@@ -2,9 +2,4 @@ return {
         "https://github.com/yilisharcs/quarrel.nvim",
         dev = true,
         cond = not vim.g.shell_editor == true,
-        init = function()
-                vim.g.quarrel = {
-                        keymaps = true,
-                }
-        end,
 }
