78690120b72f7afd77cbf72f950b3f4867ee7d41
yilisharcs <yilisharcs@gmail.com>
2026-05-23 22:33:25 -0300

    style pad

--- modules/common/shell/nushell.nix
+++ modules/common/shell/nushell.nix
@@ -98,24 +98,24 @@ in {
                     event = {
                         send = "executehostcommand";
                         cmd = /* nu */ ''commandline edit --replace (
-                                    history
-                                    | reverse
-                                    | group-by command
-                                    | values
-                                    | each { $in.0 }
-                                    | each { $"($in.index + 1)     ($in.command)" }
-                                    | str join (char -i 0)
-                                    | fzf
-                                    --read0
-                                    --layout reverse
-                                    --query (commandline)
-                                    --scheme history
-                                    --preview-window hidden
-                                    --bind='ctrl-y:execute-silent(echo -n {2..} | wl-copy)+abort'
-                                    --header 'Press CTRL-Y to copy command into clipboard'
-                                    | decode utf-8
-                                    | str trim
-                                    | str replace -r "^\\d+\\s{5}" ""
+                            history
+                            | reverse
+                            | group-by command
+                            | values
+                            | each { $in.0 }
+                            | each { $"($in.index + 1)     ($in.command)" }
+                            | str join (char -i 0)
+                            | fzf
+                            --read0
+                            --layout reverse
+                            --query (commandline)
+                            --scheme history
+                            --preview-window hidden
+                            --bind='ctrl-y:execute-silent(echo -n {2..} | wl-copy)+abort'
+                            --header 'Press CTRL-Y to copy command into clipboard'
+                            | decode utf-8
+                            | str trim
+                            | str replace -r "^\\d+\\s{5}" ""
                         )'';
                     };
                 }
