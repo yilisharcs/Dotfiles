60dc57e1352866aa741ee01afed32b090292ebbd
yilisharcs <yilisharcs@gmail.com>
2026-05-25 14:16:55 -0300

    nvim plugin quarrel.nvim: add user configuration

--- _nvim/.config/nvim/lua/plugins/quarrel.nvim.lua
+++ _nvim/.config/nvim/lua/plugins/quarrel.nvim.lua
@@ -2,4 +2,10 @@ return {
         "https://github.com/yilisharcs/quarrel.nvim",
         dev = true,
         cond = not vim.g.shell_editor == true,
+        init = function()
+                ---@type quarrel.Opts
+                vim.g.quarrel = {
+                        notify = true,
+                }
+        end,
 }
