31623dda90e3247ae1594cb0beae4ff08f4e5fc6
yilisharcs <yilisharcs@gmail.com>
2026-05-25 01:35:56 -0300

    ghostty: osc8 popups... i did not misjudge you at all!!

--- modules/common/ghostty/default.nix
+++ modules/common/ghostty/default.nix
@@ -60,8 +60,8 @@ in {
                 theme = "silverwine";
                 copy-on-select = true;
                 cursor-style = "block";
-                link-url = true;
-                link-previews = true;
+                link-url = false; # osc8 popups are annoying
+                link-previews = false;
                 mouse-hide-while-typing = true;
                 palette-generate = true;
                 palette-harmonious = false;
