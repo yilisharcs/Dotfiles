f1f4d8188e55fbc1bd8e0e1a4f37b478d56d5b2a
yilisharcs <yilisharcs@gmail.com>
2026-05-27 23:50:33 -0300

    nvim ftplugin jjdescription: work around annoying first line

--- _nvim/.config/nvim/after/ftplugin/jjdescription.lua
+++ _nvim/.config/nvim/after/ftplugin/jjdescription.lua
@@ -1,1 +1,10 @@
+local first = (vim.api.nvim_buf_get_lines(0, 0, 1, false) or {})[1]
+if first and first:match("^JJ: Enter a description for the ") then
+        vim.api.nvim_buf_set_lines(0, 0, 0, false, { "" })
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        -- no startinsert for squash
+        if first:match("combined") then
+                return
+        end
+end
 vim.cmd.startinsert()
