3141dd06ec38f7f5bbf1bd51fe253d06b17d81de
yilisharcs <yilisharcs@gmail.com>
2026-05-29 01:03:58 -0300

    forgejo: trying it out now

---
+++ modules/common/vcs/forgejo.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+  home-manager.sharedModules = [
+    {
+      home.packages = [
+        pkgs.forgejo-cli # CLI application for interacting with Forgejo
+      ];
+
+      # fj auth login
+      home.sessionVariables = {
+        FJ_FALLBACK_HOST = "https://codeberg.org";
+      };
+
+      # xdg.configFile."forgejo/cli.yml".text = ''
+      #   default_remote: origin
+      # '';
+    }
+  ];
+}
