99f349eb8335d50a11892458b299020262a5da96
yilisharcs <yilisharcs@gmail.com>
2026-05-30 23:23:29 -0300

    ssh: set explicit config

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -8,9 +8,19 @@ in {
   home-manager.sharedModules = [
     {
       programs.ssh = enabled {
-        enableDefaultConfig = false; # NOTE: will be deprecated soon
         settings = {
-          "*".addKeysToAgent = "yes";
+          "*" = {
+            AddKeysToAgent = "yes";
+            Compression = false;
+            ControlMaster = "no";
+            ControlPersist = "no";
+            ForwardAgent = false;
+            HashKnownHosts = true;
+            UserKnownHostsFile = "~/.ssh/known_hosts";
+            # ServerAliveInterval = 0;
+            # ServerAliveCountMax = 3;
+            # ControlPath = "~/.ssh/master-%r@%n:%p";
+          };
           "codeberg.org" = {
             user = "git";
             identityFile = "~/.ssh/id_ed25519_personal";
