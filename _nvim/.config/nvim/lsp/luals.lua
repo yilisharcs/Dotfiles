f43c2653effd7dd13091e78c6424ef2e1a164f3c
yilisharcs <yilisharcs@gmail.com>
2026-06-01 16:20:33 -0300

    nvim lsp: update globals

--- _nvim/.config/nvim/lsp/luals.lua
+++ _nvim/.config/nvim/lsp/luals.lua
@@ -20,7 +20,6 @@ return {
                         },
                         diagnostics = {
                                 globals = {
-                                        "vim",
                                         "P",
                                 },
                                 unusedLocalExclude = {
