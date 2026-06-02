735c6c7ea6226e87388a906ca2d9f32b8a301ef1
yilisharcs <yilisharcs@gmail.com>
2026-06-02 13:47:26 -0300

    nvim plugin cme.nvim: now with fancy syntax, qftf, and opts

--- _nvim/.config/nvim/after/ftplugin/qf.lua
+++ _nvim/.config/nvim/after/ftplugin/qf.lua
@@ -1,12 +1,6 @@
-vim.wo[0][0].list = false
 vim.wo[0][0].relativenumber = false
 vim.wo[0][0].signcolumn = "no"
 vim.wo[0][0].statuscolumn = ""
 vim.wo[0][0].wrap = false
 
 vim.wo[0][0].winhighlight = "Normal:QuickFixBg"
-
-vim.fn.matchadd("DiagnosticError", [[^\zsE\ze|]])
-vim.fn.matchadd("DiagnosticWarn", [[^\zsW\ze|]])
-vim.fn.matchadd("DiagnosticInfo", [[^\zs\(I\|N\)\ze|]])
-vim.fn.matchadd("DiagnosticHint", [[^\zsH\ze|]])
