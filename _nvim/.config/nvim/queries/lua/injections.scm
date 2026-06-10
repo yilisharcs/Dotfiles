96b4ad7a171deb9e32365d68db5438bf1a6031e4
yilisharcs <yilisharcs@gmail.com>
2026-06-10 17:21:43 -0300

    nvim query lua: inject languages freely

--- _nvim/.config/nvim/queries/lua/injections.scm
+++ _nvim/.config/nvim/queries/lua/injections.scm
@@ -1,26 +1,16 @@
 ;; extends
 
-(assignment_statement
-  (variable_list
-    name: (identifier) @_var
-    (#match? @_var "(query|parser_query|treesitter_query)"))
-  (expression_list
+; comment-annotated language injection
+;   `-- rust` before `[[...]]`
+((comment) @injection.language
+  .
+  [
     (string
       content: (string_content) @injection.content)
-    ) @_str
-  (#lua-match? @_str "^%[%[")
-  (#set! injection.language "query"))
-
-; rockspec
-(assignment_statement
-  (variable_list
-    name: (identifier) @_build1
-    (#match? @_build1 "^build$"))
     (expression_list
-      (table_constructor
-        (field
-          name: (identifier) @_build2
-          (#lua-match? @_build2 "^build_command$")
-          value: (string
-                   (string_content) @injection.content)
-          (#set! injection.language "sh")))))
+      value: (string
+        content: (string_content) @injection.content))
+  ] @_str
+  (#lua-match? @_str "^%[%[")
+  (#gsub! @injection.language "^%-%-%s*([%w_]+)%s*$" "%1")
+  (#set! injection.combined))
