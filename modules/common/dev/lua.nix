b927cef48b95792b241838bed1e33c2dc80abe57
yilisharcs <yilisharcs@gmail.com>
2026-06-11 14:25:59 -0300

    lua: friendship ended with luajit; now lua55 is my best friend

--- modules/common/dev/lua.nix
+++ modules/common/dev/lua.nix
@@ -2,7 +2,14 @@
   home-manager.sharedModules = [
     {
       home.packages = [
-        pkgs.luajit
+        # https://github.com/NixOS/nixpkgs/pull/530910
+        (pkgs.lua5_5.overrideAttrs (old: {
+          postFixup =
+            (old.postFixup or "")
+            + pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+              patchelf --add-rpath "${pkgs.readline}/lib" "$out/bin/lua"
+            '';
+        }))
         pkgs.lua-language-server
         pkgs.stylua # opinionated Lua code formatter
       ];
