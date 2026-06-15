5b092662175abc857fb145254c1f68622b4dbcd9
yilisharcs <yilisharcs@gmail.com>
2026-06-15 06:16:27 -0300

    fennel: add readline support with rlwrap

--- modules/common/dev/fennel/default.nix
+++ modules/common/dev/fennel/default.nix
@@ -1,16 +1,23 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  fennel = pkgs.lua55Packages.fennel;
+
+  fennel-wrapped = pkgs.symlinkJoin {
+    inherit (fennel) meta version;
+    pname = "fennel";
+    paths = [fennel];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      rm $out/bin/fennel
+      makeWrapper ${pkgs.rlwrap}/bin/rlwrap $out/bin/fennel \
+        --add-flags "${fennel}/bin/fennel"
+    '';
+  };
+  });
+in {
   home-manager.sharedModules = [
     {
       home.packages = [
-        # https://github.com/NixOS/nixpkgs/pull/530910
-        (pkgs.lua5_5.overrideAttrs (old: {
-          postFixup =
-            (old.postFixup or "")
-            + pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-              patchelf --add-rpath "${pkgs.readline}/lib" "$out/bin/lua"
-            '';
-        }))
-        pkgs.lua55Packages.fennel # A lisp that compiles to Lua
+        fennel-wrapped # A lisp that compiles to Lua
         pkgs.fennel-ls # Fennel LSP
         pkgs.fnlfmt # Formatter for Fennel
       ];
