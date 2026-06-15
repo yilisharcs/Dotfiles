47c66da22b31475b0cf53f424993381cdad7612e
yilisharcs <yilisharcs@gmail.com>
2026-06-15 06:16:27 -0300

    fennel: move it to its own file

--- modules/common/dev/fennel/default.nix
+++ modules/common/dev/fennel/default.nix
@@ -10,8 +10,9 @@
               patchelf --add-rpath "${pkgs.readline}/lib" "$out/bin/lua"
             '';
         }))
-        pkgs.lua-language-server
-        pkgs.stylua # opinionated Lua code formatter
+        pkgs.lua55Packages.fennel # A lisp that compiles to Lua
+        pkgs.fennel-ls # Fennel LSP
+        pkgs.fnlfmt # Formatter for Fennel
       ];
     }
   ];
