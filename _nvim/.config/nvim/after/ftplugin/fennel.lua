3c0139303083c3324ecd7582c42e7ec70a26c741
yilisharcs <yilisharcs@gmail.com>
2026-06-17 16:03:01 -0300

    nvim ftplugin fennel: extend iskeyword for valid identifiers

--- _nvim/.config/nvim/after/ftplugin/fennel.lua
+++ _nvim/.config/nvim/after/ftplugin/fennel.lua
@@ -1,1 +1,18 @@
+-- NOTE: vim.opt/_local/_global will be deprecated by v1.0
+vim.opt_local.iskeyword:append(table.concat({
+        -- idents
+        "?",
+        "!",
+        "*",
+        "-",
+        "<",
+        ">",
+        "$",
+        "&",
+        -- macros
+        "'",
+        "`",
+        "\\,",
+}, ","))
+
 vim.bo.includeexpr = [[tr(substitute(v:fname, '^:', '', ''), '.', '/')]]
