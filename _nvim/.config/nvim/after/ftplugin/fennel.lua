0b736cdedbdeabe69e64ec94ef38c6ff0726179f
yilisharcs <yilisharcs@gmail.com>
2026-06-19 06:02:51 -0300

    nvim ftplugin fennel: opt:append can't concat a comma

--- _nvim/.config/nvim/after/ftplugin/fennel.lua
+++ _nvim/.config/nvim/after/ftplugin/fennel.lua
@@ -1,18 +1,19 @@
--- NOTE: vim.opt/_local/_global will be deprecated by v1.0
-vim.opt_local.iskeyword:append(table.concat({
-        -- idents
-        "?",
-        "!",
-        "*",
-        "-",
-        "<",
-        ">",
-        "$",
-        "&",
-        -- macros
-        "'",
-        "`",
-        "\\,",
-}, ","))
+vim.bo.iskeyword = vim.o.iskeyword
+        .. ","
+        .. table.concat({
+                -- idents
+                "?",
+                "!",
+                "*",
+                "-",
+                "<",
+                ">",
+                "$",
+                "&",
+                -- macros
+                "'",
+                "`",
+                ",",
+        }, ",")
 
 vim.bo.includeexpr = [[tr(substitute(v:fname, '^:', '', ''), '.', '/')]]
