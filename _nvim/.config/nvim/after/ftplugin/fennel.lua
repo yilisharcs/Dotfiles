23dd5ea6c9087fcd6ca48676e3cc8f3fab67d54c
yilisharcs <yilisharcs@gmail.com>
2026-06-19 07:36:06 -0300

    nvim plugin mini.nvim: fennel split (no join)
    the join behavior is a tad more complicated than it looks.
    plus, indent is always added, which fnlfmt doesn't like...
    i should patch the plugin.

--- _nvim/.config/nvim/after/ftplugin/fennel.lua
+++ _nvim/.config/nvim/after/ftplugin/fennel.lua
@@ -17,3 +17,48 @@ vim.bo.iskeyword = vim.o.iskeyword
         }, ",")
 
 vim.bo.includeexpr = [[tr(substitute(v:fname, '^:', '', ''), '.', '/')]]
+
+local function split_hook(positions)
+        if #positions < 3 then
+                return positions
+        end
+
+        local first = positions[1]
+        local bracket = vim.api.nvim_buf_get_lines(0, first.line - 1, first.line, false)[1]:sub(first.col, first.col)
+
+        local seq_tbl = bracket == "["
+        local kv_tbl = bracket == "{"
+
+        if seq_tbl then
+                return positions
+        elseif kv_tbl then
+                local n = #positions
+                local result = { positions[1] }
+
+                for i = 2, n - 1 do
+                        local prev, curr = positions[i - 1], positions[i]
+                        local text = vim.api
+                                .nvim_buf_get_lines(0, prev.line - 1, prev.line, false)[1]
+                                :sub(prev.col + 1, curr.col - 1)
+                                :match("^%s*(.-)%s*$")
+                        if text ~= "" and text:sub(1, 1) ~= ":" then
+                                table.insert(result, curr)
+                        end
+                end
+
+                table.insert(result, positions[n])
+                return result
+        end
+
+        return positions
+end
+
+vim.b.minisplitjoin_config = {
+        detect = {
+                brackets = { "%b[]", "%b{}" },
+                separator = "%s+",
+        },
+        split = {
+                hooks_pre = { split_hook },
+        },
+}
