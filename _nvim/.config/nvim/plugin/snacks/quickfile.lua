6ac7de85596d00f2cf9dbf58fe835b14bd4e1776
yilisharcs <yilisharcs@gmail.com>
2026-07-01 22:47:21 -0300

    nvim quickfile: defer snacks quickfile to BufReadPost
    this ran at plugin load time (unlike the original) and set 'syntax'
    before the FileType autocmd had a chance to configure it, breaking
    my custom syntax for markdown and rust

--- _nvim/.config/nvim/plugin/snacks/quickfile.lua
+++ _nvim/.config/nvim/plugin/snacks/quickfile.lua
@@ -1,32 +1,39 @@
 -- https://github.com/folke/snacks.nvim/blob/ad9ede6a9cddf16cedbd31b8932d6dcdee9b716e/lua/snacks/quickfile.lua
 
--- Skip if we already entered vim
-if vim.v.vim_did_enter == 1 then
-        return
-end
-if vim.bo.filetype == "bigfile" then
-        return
-end
+vim.api.nvim_create_autocmd("BufReadPost", {
+        once = true,
+        callback = function()
+                -- Skip if we already entered vim
+                if vim.v.vim_did_enter == 1 then
+                        return
+                end
+                if vim.bo.filetype == "bigfile" then
+                        return
+                end
 
-local buf = vim.api.nvim_get_current_buf()
+                local buf = vim.api.nvim_get_current_buf()
 
--- Try to guess the filetype (may change later on during Neovim startup)
-local ft = vim.filetype.match({ buf = buf })
-if ft then
-        -- Add treesitter highlights and fallback to syntax
-        local lang = vim.treesitter.language.get_lang(ft)
+                -- Try to guess the filetype (may change later on during Neovim startup)
+                local ft = vim.filetype.match({ buf = buf })
+                if ft then
+                        -- Add treesitter highlights and fallback to syntax
+                        local lang = vim.treesitter.language.get_lang(ft)
 
-        -- disable treesitter for some langs
-        if vim.tbl_contains({
-                "latex",
-        }, lang) then
-                lang = nil
-        end
+                        -- disable treesitter for some langs
+                        if
+                                vim.tbl_contains({
+                                        "latex",
+                                }, lang)
+                        then
+                                lang = nil
+                        end
 
-        if not (lang and pcall(vim.treesitter.start, buf, lang)) then
-                vim.bo[buf].syntax = ft
-        end
+                        if not (lang and pcall(vim.treesitter.start, buf, lang)) then
+                                vim.bo[buf].syntax = ft
+                        end
 
-        -- Trigger early redraw
-        vim.cmd([[redraw]])
-end
+                        -- Trigger early redraw
+                        vim.cmd([[redraw]])
+                end
+        end,
+})
