309d584319c31a64e820c0295fdddb67c9aaaf98
yilisharcs <yilisharcs@gmail.com>
2026-07-02 01:57:50 -0300

    nix: scope rec where it should be

--- lib/colors.nix
+++ lib/colors.nix
@@ -1,9 +1,4 @@
-rec {
-  hexToRgb = hex: let
-    h = builtins.replaceStrings ["#"] [""] hex;
-    p = s: (builtins.fromTOML "a = 0x${s}").a;
-  in "${toString (p (builtins.substring 0 2 h))},${toString (p (builtins.substring 2 2 h))},${toString (p (builtins.substring 4 2 h))}";
-
+{
   silverwine = {
     background = "#0a001a";
     foreground = "#cdd6f4";
@@ -25,27 +20,33 @@ rec {
     white = "#cdd6f4";
   };
 
-  toKmsconPalette = scheme: {
-    palette = "custom";
-    palette-background = hexToRgb scheme.background;
-    palette-foreground = hexToRgb scheme.foreground;
-    palette-black = hexToRgb scheme.black;
-    palette-red = hexToRgb scheme.red;
-    palette-green = hexToRgb scheme.green;
-    palette-yellow = hexToRgb scheme.yellow;
-    palette-blue = hexToRgb scheme.blue;
-    palette-magenta = hexToRgb scheme.magenta;
-    palette-cyan = hexToRgb scheme.cyan;
-    palette-light-grey = hexToRgb scheme.lightGrey;
-    palette-dark-grey = hexToRgb scheme.darkGrey;
-    palette-light-red = hexToRgb scheme.lightRed;
-    palette-light-green = hexToRgb scheme.lightGreen;
-    palette-light-yellow = hexToRgb scheme.lightYellow;
-    palette-light-blue = hexToRgb scheme.lightBlue;
-    palette-light-magenta = hexToRgb scheme.lightMagenta;
-    palette-light-cyan = hexToRgb scheme.lightCyan;
-    palette-white = hexToRgb scheme.white;
-  };
+  toKmsconPalette = let
+    hexToRgb = hex: let
+      h = builtins.replaceStrings ["#"] [""] hex;
+      p = s: (builtins.fromTOML "a = 0x${s}").a;
+    in "${toString (p (builtins.substring 0 2 h))},${toString (p (builtins.substring 2 2 h))},${toString (p (builtins.substring 4 2 h))}";
+  in
+    scheme: {
+      palette = "custom";
+      palette-background = hexToRgb scheme.background;
+      palette-foreground = hexToRgb scheme.foreground;
+      palette-black = hexToRgb scheme.black;
+      palette-red = hexToRgb scheme.red;
+      palette-green = hexToRgb scheme.green;
+      palette-yellow = hexToRgb scheme.yellow;
+      palette-blue = hexToRgb scheme.blue;
+      palette-magenta = hexToRgb scheme.magenta;
+      palette-cyan = hexToRgb scheme.cyan;
+      palette-light-grey = hexToRgb scheme.lightGrey;
+      palette-dark-grey = hexToRgb scheme.darkGrey;
+      palette-light-red = hexToRgb scheme.lightRed;
+      palette-light-green = hexToRgb scheme.lightGreen;
+      palette-light-yellow = hexToRgb scheme.lightYellow;
+      palette-light-blue = hexToRgb scheme.lightBlue;
+      palette-light-magenta = hexToRgb scheme.lightMagenta;
+      palette-light-cyan = hexToRgb scheme.lightCyan;
+      palette-white = hexToRgb scheme.white;
+    };
 
   toGhosttyPalette = scheme: [
     "0=${scheme.black}"
