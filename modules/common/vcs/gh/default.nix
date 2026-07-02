b50da4b32a7c3bdae83721cef6d6cc4d02348da8
yilisharcs <yilisharcs@gmail.com>
2026-07-02 07:41:49 -0300

    gh: simplify and cleanup

--- modules/common/vcs/gh/default.nix
+++ modules/common/vcs/gh/default.nix
@@ -6,15 +6,15 @@
 }: let
   inherit (lib) enabled getExe;
 
+  # NOTE: some `gh auth` subcommands don't play nice with GH_TOKEN being set.
+  #       "auth status" and "auth token" will necessarily be out of sync if the
+  #       token is changed after a rebuild.
   gh-wrapped = pkgs.writeShellScriptBin "gh" ''
-    export GH_TOKEN=$(< ${config.age.secrets.github-cli.path})
+    case "$1 $2" in
+      "auth login" | "auth logout" | "auth switch" | "auth refresh") ;;
+      *) export GH_TOKEN=$(< ${config.age.secrets.github-cli.path})  ;;
+    esac
     exec ${getExe pkgs.gh} "$@"
-  '';
-
-  # gh auth commands don't play nice with GH_TOKEN being set. for days like these,
-  # bypass the wrapper to access the keyring directly, to rotate keys or whatever
-  gh-auth = pkgs.writeShellScriptBin "gha" ''
-    exec ${getExe pkgs.gh} auth "$@"
   '';
 in {
   age.secrets.github-cli = {
@@ -25,8 +25,6 @@ in {
 
   home-manager.sharedModules = [
     {
-      home.packages = [gh-auth];
-
       # GitHub CLI tool
       programs.gh = enabled {
         package = gh-wrapped;
@@ -36,28 +34,13 @@ in {
           git_protocol = "ssh";
         };
         settings = {
-          # What protocol to use when performing git operations.
-          # Supported values: ssh, https
           git_protocol = "ssh";
-          # What editor gh should run when creating issues, pull requests,
-          # etc. If blank, will refer to environment.
-          editor = "nvim";
-          # When to interactively prompt. This is a global config that cannot
-          # be overridden by hostname. Supported values: enabled, disabled
+          # editor = "";
           prompt = "enabled";
-          ## A pager program to send command output to, e.g. "less". If blank,
-          ## will refer to environment. Set the value to "cat" to disable the pager.
           pager = "${getExe pkgs.less}";
-          ## Aliases allow you to create nicknames for gh commands
           # aliases = {
           #         co = "pr checkout";
           # };
-          ## The path to a unix socket through which send HTTP connections.
-          ## If blank, HTTP traffic will be handled by net/http.DefaultTransport.
-          # http_unix_socket = "";
-          ## What web browser gh should use when opening URLs. If blank, will
-          ## refer to environment.
-          # browser = "";
         };
       };
     }
