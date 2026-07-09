1b85e4c6577b3cd557224ca09539a05fb40561d9
yilisharcs <yilisharcs@gmail.com>
2026-07-09 12:21:12 -0300

    nvim plugin canola.nvim: symlink keymaps for extra convenience

--- _nvim/.config/nvim/plugin/pack-conf/canola.nvim.lua
+++ _nvim/.config/nvim/plugin/pack-conf/canola.nvim.lua
@@ -1,3 +1,58 @@
+local function create_symlink(relative)
+        local source = vim.fn.getreg(vim.v.register)
+        if source:sub(1, 1) ~= "/" then
+                vim.notify(
+                        "Not an absolute path: " .. (#source == 0 and "'(empty string)'" or source),
+                        vim.log.levels.WARN
+                )
+                return
+        end
+
+        local oil_dir = require("oil").get_current_dir()
+
+        -- `vim.fs.basename` returns an empty string for paths
+        -- with a trailing slash aka directories; chop it off
+        local abs_source = source:gsub("/$", "")
+        local name = vim.fs.basename(abs_source)
+
+        local target = abs_source
+        if relative then
+                -- split paths
+                local from_parts = vim.split(oil_dir:gsub("/$", ""):sub(2), "/")
+                local to_parts = vim.split(abs_source:gsub("/$", ""):sub(2), "/")
+
+                -- find where the paths diverge
+                local common = 0
+                for i = 1, math.min(#from_parts, #to_parts) do
+                        if from_parts[i] == to_parts[i] then
+                                common = i
+                        else
+                                break
+                        end
+                end
+
+                -- number of ../ needed to reach common ancestor
+                local ups = #from_parts - common
+                -- remaining path components to reach target
+                local downs = { unpack(to_parts, common + 1) }
+                -- build relative path
+                target = string.rep("../", ups) .. table.concat(downs, "/")
+        end
+
+        -- deduplicate
+        local link_name = name
+        local i = 1
+        while vim.uv.fs_stat(oil_dir .. link_name) do
+                link_name = name .. "_" .. i
+                i = i + 1
+        end
+
+        local entry = link_name .. " -> " .. target
+        local lines = vim.api.nvim_buf_get_lines(0, -2, -1, false)
+        table.insert(lines, entry)
+        vim.api.nvim_buf_set_lines(0, -2, -1, false, lines)
+end
+
 local function goto_dir(path, desc)
         return { "<CMD>edit " .. path .. "<CR>", nowait = true, desc = desc or ("Go to " .. path) }
 end
@@ -25,7 +80,6 @@ require("oil").setup({
                 end,
                 case_insensitive = true,
         },
-
         keymaps = {
                 ["<leader>;"] = {
                         function()
@@ -33,11 +87,6 @@ require("oil").setup({
                                 vim.api.nvim_feedkeys(":MXCompile cd " .. dir .. " && ", "n", false)
                         end,
                         desc = "Open MXCompile with the current directory as an argument",
-                },
-                ["<leader>y"] = {
-                        "actions.yank_entry",
-                        mode = "n",
-                        nowait = true,
                 },
                 ["_"] = {
                         "actions.open_cwd",
@@ -67,6 +116,28 @@ require("oil").setup({
                         mode = "n",
                         nowait = true,
                         desc = "List tracked files from current directory",
+                },
+                -- symlink
+                ["<leader>y"] = {
+                        "actions.yank_entry",
+                        mode = "n",
+                        nowait = true,
+                },
+                ["<leader>p"] = {
+                        function()
+                                create_symlink(false)
+                        end,
+                        mode = "n",
+                        nowait = true,
+                        desc = "Create absolute symlink from yanked path",
+                },
+                ["<leader>P"] = {
+                        function()
+                                create_symlink(true)
+                        end,
+                        mode = "n",
+                        nowait = true,
+                        desc = "Create relative symlink from yanked path",
                 },
                 -- goto
                 ["gf"] = {
